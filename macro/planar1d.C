abd7ffe017d293c74f525ca0f67d4544a98da2b1
Jianchen Li <byron.li@usd.edu>
2016-07-22 10:42:26 -0500

    olds

--- macro/planar1d.C
+++ macro/planar1d.C
@@ -4,11 +4,11 @@ void planar1d()
 {
    PlanarX *detector = new PlanarX(11);
    detector->Thickness=10*cm;
-   detector->MaxIterations=1e6;
+   detector->MaxIterations=1e5;
    detector->Csor=1.9;
    detector->SetVoltage(0*volt, 2000*volt);
    detector->SetImpurity(1e10/cm3);
-   detector->CalculateField(EMethod::kRK4);
+   detector->CalculateField(EMethod::kRK2);
    detector->SaveField("planar1dRK2.root");
    detector->CalculateField(EMethod::kAnalytic);
    detector->SaveField("planar1dTrue.root");
