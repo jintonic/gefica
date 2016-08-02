57ff768337c67e6f9a352ba14c17af129fe5deaf
Jianchen Li <byron.li@usd.edu>
2016-08-02 10:48:28 -0500

    bug fixed

--- core/Polar1d.cc
+++ core/Polar1d.cc
@@ -22,7 +22,7 @@ bool Polar1d::Analyic()
    double cnst1=(fPotential[n-1]-fPotential[0]-density*(fC1[n-1]*fC1[n-1]-fC1[0]*fC1[0])/epsilon/4)/(log(fC1[n-1]/fC1[0]));
    double cnst2=fPotential[0]-density*fC1[0]*fC1[0]/epsilon/4-cnst1*log(fC1[0]);
    for (int i=0; i<n; i++) {
-      fPotential[i] = fImpurity[i]*1.6e-19/4/epsilon*fC1[i]*fC1[i]+cnst1*log(fC1[i])-cnst2;
+      fPotential[i] = fImpurity[i]*1.6e-19/4/epsilon*fC1[i]*fC1[i]+cnst1*log(fC1[i])+cnst2;
       fE1[i]=(fPotential[i+1]-fPotential[i-1])/(fDistanceToNext[i]+fDistanceToPrevious[i]);
    }
    return true;
