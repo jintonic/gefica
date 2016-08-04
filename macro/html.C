8bf61d2a1a0ee648f37f04b887d7fa088e62cd2a
Jing LIU <jing.liu@usd.edu>
2016-08-04 09:15:57 -0500

    fixed some bugs

--- macro/html.C
+++ macro/html.C
@@ -5,9 +5,9 @@ void html()
   X x;
   THtml html;
   html.SetProductName("GEFICA");
-  html.SetHomepage("htt[://github.com");
-  html.SetCharset("UTF8");
-  html.SetAuthorTag("Jiu Jing, Jianchen Li");
+  html.SetHomepage("http://github.com/jintonic/gefica");
+  html.SetCharset("UTF-8");
+  html.SetAuthorTag("Jing Liu, Jianchen Li");
   html.SetInputDir("../core");
   html.SetOutputDir("../doc/html");
   html.MakeAll();
