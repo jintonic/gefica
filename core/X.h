f3d6b8bb6741d614f5315e27fd0e134c3d67bc71
Mitchell Wagner <mitch_wagner@live.com>
2016-10-19 11:53:32 -0500

    Finished adding comments to X.h

--- core/X.h
+++ core/X.h
@@ -42,30 +42,44 @@ class GeFiCa::X : public TObject
       X(int nx=101);
 
       virtual ~X();
-	  /**
-	  * Function for deciding which method to use for the field calculation. 
-	  * Current methods are kAnalytic and kSOR2.
+	   /**
+	   * Function for deciding which method to use for the field calculation. 
+	   * Current methods are kAnalytic and kSOR2.
       */
       virtual bool CalculateField(EMethod method=kSOR2);
       /**
-	  * This function creates a new TFile and TTree and fills it from data created by X::CalculateField.    
-	  */
+	   * This function creates a new TFile and TTree and fills it from data created by X::CalculateField.    
+	   */
       virtual void SaveField(const char *fout);
       /**
-	  * calculate electric field after load
-	  */
-	  virtual void LoadField(const char *fin);
-	  /*! \brief Ionizing impurity level method
-	  * 
-	  * This function takes an argument for the variable density.
-	  * It is usualy in the form of 1e10/cm3 or some variation.
-	  */
+	   * calculate electric field after load
+	   */
+	   virtual void LoadField(const char *fin);
+	   /*! \brief Ionizing impurity level method
+	   * 
+	   * This function takes an argument for the variable density.
+	   * It is usualy in the form of 1e10/cm3 or some variation.
+	   */
       virtual void SetImpurity(double density);
+      /**
+      * Another Important method involved in setting the impurity.
+      */
       virtual void SetImpurity(TF1 * Im);
-
-      virtual double GetE1(double x){return GetData(x,2);}; 
-      virtual double GetImpurity(double x){return GetData(x,0);}; 
-      virtual double GetPotential(double x){return GetData(x,1);}; 
+      /**
+      * Returns the value for E under the first direction.
+      */
+      virtual double GetE1(double x){return GetData(x,2);};
+      /**
+      * Returns the impurity level.
+      */ 
+      virtual double GetImpurity(double x){return GetData(x,0);};
+      /**
+      * Returns the potential.
+      */ 
+      virtual double GetPotential(double x){return GetData(x,1);};
+      /**
+      * Returns the location under the first directions.
+      */ 
       virtual double GetXEdge(bool beginorend); 
 
       ClassDef(X,1);
@@ -77,13 +91,28 @@ class GeFiCa::X : public TObject
       double  *fPotential; // Potential in this grid
       double *fC1; // the location under first direction
       double *fDistanceToNext,*fDistanceToPrevious,*fImpurity;
-
+      /**
+      * Sets the field step length.
+      */
       virtual void SetStepLength(double steplength);
+      /**
+      * Uses a binary search to return the index .
+      */
       virtual int FindIdx(double tarx,int begin,int end);
-
+      /**
+      * Calculates the field using an analytical method.
+      */
       virtual bool Analytic();
-      
+      /**
+      * Returns data for various variables. 
+      * Case 0: Returns impurity
+      * Case 1: Returns the potential
+      * Case 2: Returns E1
+      */
       virtual double GetData(double tarx,int thing); 
+      /**
+      * Uses Second order Successive Over-Relaxation method to calculate the field.
+      */
       virtual void SOR2(int idx,bool calculateElectricField); 
       
 };
