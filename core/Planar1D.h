2d058fbf4368d38091396f8b4d552290b645cf25
Jing LIU <jing.liu@usd.edu>
2016-11-02 16:42:28 -0500

    fixed bug introduced by documentation

--- core/Planar1D.h
+++ core/Planar1D.h
@@ -5,33 +5,34 @@
 
 namespace GeFiCa { class Planar1D; }
 /**
-*1 D planar detector geometry for which the static electronic potential and field are calculated.
-*/
+ * 1 D planar detector geometry for which the static electronic potential and
+ * field are calculated.
+ */
 class GeFiCa::Planar1D : public GeFiCa::X
 {
    public :
-      double UpperBound,/**< upper boundary of the planar detector.*/LowerBound;/**< lower boundary of the planar detector.*/ 
-      double cathode_voltage/**< Volage of the cathode. */,annode_voltage;/**< Voltage of the anode. */
+      double UpperBound;///< upper boundary of the planar detector
+      double LowerBound;///< lower boundary of the planar detector
+      double cathode_voltage;///< Volage of the cathode
+      double annode_voltage;///< Voltage of the anode
 
    public :
-   /**
-   *The constructor for Planar1D, of given a value for nx, no input is needed.
-   */
-      Planar1D(int nx=101) : X(nx),
-	UpperBound(10),/**< Input upper boundary of the planar detector. */
-	LowerBound(1),/**< Input lower boundary of the planar detector. */
-	cathode_voltage(2000),/**< Volage of the cathode. */
-	annode_voltage(0)/**< Voltage of the anode. */ {}; 
-	
-	  /**
-	  *Calculates the step length for the detector
-	  */
+      /**
+       * The constructor for Planar1D, of given a value for nx, no input is
+       * needed.
+       */
+      Planar1D(int nx=101) : X(nx), UpperBound(10), LowerBound(1), 
+      cathode_voltage(2000), annode_voltage(0){}; 
+
+      /**
+       *Calculates the step length for the detector
+       */
       void initialize(); 
-	  bool Analytic();
+      bool Analytic();
       bool CalculateField(EMethod method=kSOR2);
-	  /**
-	  *This defines the class Planar1D for the cint dictionary.
-	  */
+      /**
+       *This defines the class Planar1D for the cint dictionary.
+       */
       ClassDef(Planar1D, 1);
 };
 
