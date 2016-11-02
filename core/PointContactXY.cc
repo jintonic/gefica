35fd9be7e851a49f164dbf9e105698bb66819aa9
Jing LIU <jing.liu@usd.edu>
2016-11-02 16:40:37 -0500

    corrected wrong idx in initialization

--- core/PointContactXY.cc
+++ core/PointContactXY.cc
@@ -1,22 +1,9 @@
 #include "PointContactXY.h"
 using namespace GeFiCa;
-//_______________________________
-//
-//it look like this:
-//____________________
-//|                  |
-//|                  |
-//|                  |
-//|                  |
-//|                  |
-//|                  |
-//|                  |
-//|                  |
-//|       ____       |
-ClassImp(PointContactXY)
+
 void PointContactXY::initialize()
 {
-  // The step length is calculated with the following equation:
+   // The step length is calculated with the following equation:
    // BEGIN_HTML
    // <pre>
    //      double stepLength=(UpperBound-LowerBound)/(n-1);
@@ -25,33 +12,33 @@ void PointContactXY::initialize()
    // If the inner radius is not larger than the outer radius,
    // no grid will be created
    if (XLowerBound>=XUpperBound||YLowerBound>=YUpperBound) {
-      Warning("CreateGridWithFixedStepLength",
+      Warning("initialize",
             "Lower bound (%f) >= upper bound (%f)! No grid is created!",
             XLowerBound, XUpperBound);
       return;
    }
-  double steplength1=(XUpperBound-XLowerBound)/n1;
-  double steplength2=(YUpperBound-YLowerBound)/n2;
-  XY::SetStepLength(steplength1,steplength2);
-  //might have a problem here
-  for(int i=n;i-->n-n1;) {
+   double steplength1=(XUpperBound-XLowerBound)/n1;
+   double steplength2=(YUpperBound-YLowerBound)/n2;
+   SetStepLength(steplength1,steplength2);
+   // set potential for electrodes
+   for(int i=n-1;i>=n-n1;i--) {
       fIsFixed[i]=true;
       fPotential[i]=annode_voltage;
-      if(fC1[n-i]>=PointBegin&&fC1[n-i]<=PointEnd) 
-      {
-	fPotential[n-i]=cathode_voltage;
-	fIsFixed[n-i]=true;
+      if(fC1[n-1-i]>=PointBegin&&fC1[n-1-i]<=PointEnd) {
+         fPotential[n-1-i]=cathode_voltage;
+         fIsFixed[n-1-i]=true;
       }
-  }
-  for(int i=0;i<n-n1;i=i+n1) {
+   }
+   for(int i=0;i<n-n1;i=i+n1) {
       fIsFixed[i]=true;
       fIsFixed[i+n1-1]=true;
       fPotential[i]=annode_voltage;
       fPotential[i+n1-1]=annode_voltage;
-  }
+   }
 }
+
 bool PointContactXY::CalculateField(EMethod method)
 {
-  if(!fIsLoaded)initialize();
-  return XY::CalculateField(method);
+   if(!fIsLoaded)initialize();
+   return CalculateField(method);
 }
