b5538bec6b5a08cc16549c6666df0a6954be2551
Mitchell Wagner <mitch_wagner@live.com>
2016-11-03 12:34:07 -0500

    Commented through XY.h

--- core/XY.h
+++ core/XY.h
@@ -16,37 +16,71 @@ namespace GeFiCa { class XY; }
 class GeFiCa::XY : public GeFiCa::X
 {
    public:
-      unsigned short n2; // number of steps along the 2nd axis
-
+      unsigned short n2; /**< number of steps along the 2nd axis*/
    public:
+      /**
+      * XY is a constructor with default values n1,n2 = 101
+      */
       XY(unsigned short n1=101, unsigned short n2=101);
       
       virtual ~XY();
 
-
       virtual void SaveField(const char *fout=NULL);
       virtual void LoadField(const char *fin=NULL);
-
+		/**
+		* Returns the potential in two dimensions
+		*/
       virtual double GetPotential(double x,double y){return GetData(x,y,1);};
+      /**
+      * Returns the value for the electric field under the first direction
+      */
       virtual double GetE1(double x,double y){return GetData(x,y,2);};
+      /**
+      * Returns the value for the electic field under the second direction
+      */
       virtual double GetE2(double x,double y){return GetData(x,y,3);};
+      /**
+      * Returns the two dimensional impurity
+      */
       virtual double GetImpurity(double x,double y){return GetData(x,y,0);};
 
-   
+   	/**
+   	* Method involved in setting the impurity. 
+   	* This is used for a variable impurity level that changes with x
+   	*/
       virtual void SetImpurity(TF2 * Im);
       virtual void SetImpurity(double density);
-
+		/**
+		* This defines the class for the CINT library
+		*/
       ClassDef(XY,1);
 
    protected:
 
-      virtual void SOR2(int idx,bool elec); 
-      double *fE2,*fC2,*fDistanceToLeft,*fDistanceToRight;//left and right are for y axis
+      virtual void SOR2(int idx,bool elec);
+      double *fE2; /**< Electric field under the second coordinate (x, r, or rho) direction */
+      double *fC2; /**< The location under the second coordinate (x, r, or rho) direction*/
+      double *fDistanceToLeft; /**< Distance to the next point to the left under the current coordinate 
+      direction */
+      double *fDistanceToRight;/**< Distance to the next point to the right under the current coordinate 
+      direction*/ 
+      //left and right are for y axis
+      /**
+      * Uses a binary search to return the index in two dimensions
+      */
       virtual int FindIdx(double tarx,double tary
             ,int ybegin,int yend);
 
 
-      double GetData(double tarx,double tary,int thing);
-      virtual void SetStepLength(double steplength1,double steplength2);
+
+		/**
+      * Returns data for various variables. 
+      * Case 0: Returns impurity 
+      * Case 1: Returns the potential 
+      *Case 2: Returns E1
+      */
+      double GetData(double tarx,double tary,int thing); 
+      virtual void SetStepLength(double steplength1,double steplength2); 
 };
-#endif
+
+#endif 
--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['x',['X',['../classGeFiCa_1_1X.html',1,'GeFiCa']]],
-  ['x',['X',['../classGeFiCa_1_1X.html#aa3817680a2d63fe5b3af36d54244555c',1,'GeFiCa::X']]],
+  ['x',['X',['../classGeFiCa_1_1X.html#a80ffb7e5f090060e494a2e0d42b4a2e4',1,'GeFiCa::X']]],
   ['xy',['XY',['../classGeFiCa_1_1XY.html',1,'GeFiCa']]],
+  ['xy',['XY',['../classGeFiCa_1_1XY.html#a057f3cd5cee3be6d171b34e6dd3e441b',1,'GeFiCa::XY']]],
   ['xyz',['XYZ',['../classGeFiCa_1_1XYZ.html',1,'GeFiCa']]]
 ];
