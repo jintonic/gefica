bbe17afeb21fde92bfd414da5eea32871b11b4cf
Jing LIU <jing.liu@usd.edu>
2016-12-05 18:01:27 -0600

    moved definition of units out

--- core/X.h
+++ core/X.h
@@ -5,26 +5,14 @@
 class TF1;
 
 namespace GeFiCa { 
-
-	/**
-	* EMethod is a namespace with three different methods of field calculation.
-	*/
-   enum EMethod {
-      kAnalytic,	/**< Analytical method*/
-      kSOR2,		/**< Successove over-relaxation method to the 2nd order*/
-      kSOR4,		/**< Successove over-relaxation method to the 4th order*/
+   enum EMethod ///< Different methods to calculate fields
+   {
+      kAnalytic,
+      kSOR2, ///< Successove over-relaxation method to the 2nd order
+      kSOR4, ///< Successove over-relaxation method to the 4th order
    };
 
    class X;
-
-
-   static const double Coulomb=1;
-   static const double Qe = 1.6e-19;  ///< electron charge in Coulomb
-   static const double cm =1;
-   static const double cm3=cm*cm*cm; 
-   static const double meter=100*cm;
-   static const double volt=1;
-   static const double epsilon = 16*8.854187817e-12*Coulomb/volt/meter; ///< dielectric constant of Ge
 }
 
 /**
@@ -46,57 +34,57 @@ class GeFiCa::X : public TObject
       int t,d;
 
    public:
-   
+
       /**
-	  * X is a constructor, if given a number, no input is needed
-	  */
+       * X is a constructor, if given a number, no input is needed
+       */
       X(int nx=101 /**< Number of grid lines. */);
 
       virtual ~X();
-	   /**
-	   * Function for deciding which method to use for the field calculation. 
-	   * Current methods are kAnalytic and kSOR2.
-      */
+      /**
+       * Function for deciding which method to use for the field calculation. 
+       * Current methods are kAnalytic and kSOR2.
+       */
       virtual bool CalculateField(EMethod method=kSOR2);
       /**
-	   * This function creates a new TFile and TTree and fills it from data created by X::CalculateField.    
-	   */
+       * This function creates a new TFile and TTree and fills it from data created by X::CalculateField.    
+       */
       virtual void SaveField(const char *fout);
       /**
-	   * calculate electric field after load
-	   */
-	   virtual void LoadField(const char *fin);
-	   /*! \brief Ionizing impurity level method
-	   * 
-	   * This function takes an argument for the variable density. It can be used if you consider impurity to be constant.
-	   * It is usualy in the form of 1e10/cm3 or some variation where cm3 is defined in the namespace GeFiCa.
-	   */
+       * calculate electric field after load
+       */
+      virtual void LoadField(const char *fin);
+      /*! \brief Ionizing impurity level method
+       * 
+       * This function takes an argument for the variable density. It can be used if you consider impurity to be constant.
+       * It is usualy in the form of 1e10/cm3 or some variation where cm3 is defined in the namespace GeFiCa.
+       */
       virtual void SetImpurity(double density); // If you can consider impurity to e constant
       /**
-      * Another Important method involved in setting the impurity. This is used for a variable impurity level that changes with x.
-      */
+       * Another Important method involved in setting the impurity. This is used for a variable impurity level that changes with x.
+       */
       virtual void SetImpurity(TF1 * Im); // Used for the real change in impurity over x 
       /**
-      * Returns the value for E under the first direction.
-      */
+       * Returns the value for E under the first direction.
+       */
       virtual double GetE1(double x,double y,double z){return GetData(x,2);};
       virtual double GetE2(double x,double y,double z){return 0;};
       virtual double GetE3(double x,double y,double z){return 0;};
       /**
-      * Returns the impurity level.
-      */ 
+       * Returns the impurity level.
+       */ 
       virtual double GetImpurity(double x){return GetData(x,0);};
       /**
-      * Returns the potential.
-      */ 
+       * Returns the potential.
+       */ 
       virtual double GetPotential(double x){return GetData(x,1);};
       /**
-      * Returns the location under the first directions.
-      */ 
+       * Returns the location under the first directions.
+       */ 
       virtual double GetXEdge(bool beginorend); 
-	  /**
-	  *This defines the class for the cint dictionary.
-	  */
+      /**
+       *This defines the class for the cint dictionary.
+       */
       ClassDef(X,1);
 
    protected:
@@ -106,33 +94,33 @@ class GeFiCa::X : public TObject
       double  *fPotential; ///< Potential in the referenced grid
       double *fC1; ///< the location under the first coordinate (x, r, or rho) direction
       double *fDistanceToNext;///< Distance to the next point under the current coordinate direction
-	  double *fDistanceToPrevious;///< Distance to the previous point under the current coordinate direction
-	  double *fImpurity; ///< Value of the impurity level at a point on the grid
-	  
+      double *fDistanceToPrevious;///< Distance to the previous point under the current coordinate direction
+      double *fImpurity; ///< Value of the impurity level at a point on the grid
+
       /**
-      * Sets the field step length.
-      */
+       * Sets the field step length.
+       */
       virtual void SetStepLength(double steplength);
       /**
-      * Uses a binary search to return the index .
-      */
+       * Uses a binary search to return the index .
+       */
       virtual int FindIdx(double tarx,int begin,int end);
       /**
-      * Calculates the field using an analytical method.
-      */
+       * Calculates the field using an analytical method.
+       */
       virtual bool Analytic();
       /**
-      * Returns data for various variables. 
-      * Case 0: Returns impurity
-      * Case 1: Returns the potential
-      * Case 2: Returns E1
-      */
+       * Returns data for various variables. 
+       * Case 0: Returns impurity
+       * Case 1: Returns the potential
+       * Case 2: Returns E1
+       */
       virtual double GetData(double tarx,int thing); 
       /**
-      * Uses Second order Successive Over-Relaxation method to calculate the field.
-      */
+       * Uses Second order Successive Over-Relaxation method to calculate the field.
+       */
       virtual void SOR2(int idx,bool calculateElectricField); 
-      
+
 };
 #endif
 
