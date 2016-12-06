4a646c0a092ae524c3443ec2baeae4a1becf0124
Jing LIU <jing.liu@usd.edu>
2016-12-05 18:01:35 -0600

    added

---
+++ core/Units.h
@@ -0,0 +1,13 @@
+#ifndef GeFiCa_UNITS_H
+#define GeFiCa_UNITS_H
+
+namespace GeFiCa { 
+   static const double epsilon = 16*8.854187817e-14; ///< dielectric constant of Ge [C/volt/cm]
+   static const double Qe = 1.6e-19;  ///< electron charge in Coulomb [C]
+   static const double cm =1;
+   static const double cm3=cm*cm*cm;
+   static const double volt=1;
+}
+
+#endif
+
