8b40404861e3df79cbd57ddb3867a71dc714e488
Jing LIU <jing.liu@usd.edu>
2016-12-05 18:02:31 -0600

    added

---
+++ macro/sphere.C
@@ -0,0 +1,27 @@
+using namespace GEFICA;
+
+void sphere()
+{
+   ball *detector = new ball(11,11,11);
+   detector->MaxIterations=1e5;
+   detector->Csor=1.95;
+   detector->Create(0.3,3);
+   detector->SetVoltage(0*volt,10*volt);
+   detector->CalculateField(EMethod::kRK2);
+   detector->SaveField("planar1dRK2.root");
+
+   Sph *detector2 = new Sph(110);
+   detector2->MaxIterations=1e5;
+   detector2->Csor=1.9;
+   detector2->Create(0.3,3);
+   detector2->SetVoltage(0*volt, 10*volt);
+   detector2->CalculateField(EMethod::kAnalytic);
+   detector2->SaveField("planar1dTrue.root");
+
+   TChain *t1=new TChain("t");
+   t1->Add ("planar1dRK2.root");
+   TChain *t2=new TChain("t");
+   t2->Add("planar1dTrue.root");
+   t1->Draw("p:c1");
+   t2->Draw("p:c1","","same");
+}
