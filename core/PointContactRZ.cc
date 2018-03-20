6c0f5a7ff218451267b7d2fa8517416b333d061d
Jing LIU <jing.liu@usd.edu>
2018-03-20 11:51:04 -0500

    moved inner grid points to pc boundary

--- core/PointContactRZ.cc
+++ core/PointContactRZ.cc
@@ -9,50 +9,51 @@ using namespace std;
 //the current design does not include when point contact is close to boundary of the whole detector.
 void PointContactRZ::BounardaryOnPointcontact()
 {
-    int index=FindIdx(PointR,PointDepth,0,n2-1);
-    cout<<index<<" "<<fC1[index]<<" "<<fC2[index]<<endl;
-    int idxZ=index/n1*n1;
-    int idxPos=index%n1;
-    cout<<idxPos<<" "<<fC1[idxPos]<<" "<<fC2[idxPos]<<endl;
-    cout<<idxZ<<" "<<fC1[idxZ]<<" "<<fC2[idxZ]<<endl;
+   int index=FindIdx(PointR,PointDepth,0,n2-1);
+   cout<<index<<" "<<fC1[index]<<" "<<fC2[index]<<endl;
+   int idxZ=index/n1*n1-1;
+   int idxPos=index%n1-1;
+   cout<<idxPos<<" "<<fC1[idxPos]<<" "<<fC2[idxPos]<<endl;
+   cout<<idxZ<<" "<<fC1[idxZ]<<" "<<fC2[idxZ]<<endl;
 
-    for (int i=0;i<n1;i++) {
-        fC2[idxZ+i]=PointDepth;//set z for the line where z is closest to pc's depth
-        //cout<<idxZ+i<<" "<< fC2[idxZ+i]<<"| ";
+   for (int i=0;i<n1;i++) {
+      if (PointDepth==0) break;
+      fC2[idxZ+i]=PointDepth;//set z for the line where z is closest to pc's depth
+      //cout<<idxZ+i<<" "<< fC2[idxZ+i]<<"| ";
 
-        //set steps from depthline
-        fDistanceToLeft[idxZ+i]=fC2[idxZ+i]-fC2[idxZ+i-n1];
-        fDistanceToRight[idxZ+i]=fC2[idxZ+i+n1]-fC2[idxZ+i];
+      //set steps from depthline
+      fDistanceToLeft[idxZ+i]=fC2[idxZ+i]-fC2[idxZ+i-n1];
+      fDistanceToRight[idxZ+i]=fC2[idxZ+i+n1]-fC2[idxZ+i];
 
-        //set steps for two line aside depth of pc
-        fDistanceToLeft[idxZ+i+n1]=fDistanceToRight[idxZ+i];
-        fDistanceToRight[idxZ+i-n1]=fDistanceToLeft[idxZ+i];
-    }
+      //set steps for two line aside depth of pc
+      fDistanceToLeft[idxZ+i+n1]=fDistanceToRight[idxZ+i];
+      fDistanceToRight[idxZ+i-n1]=fDistanceToLeft[idxZ+i];
+   }
 
-    for(int i=0;i<n2;i++) {
-        fC1[idxPos+i*n1]=PointR;//set r for the line where r is closest to pc's R 
+   for(int i=0;i<n2;i++) {
+      fC1[idxPos+i*n1]=PointR;//set r for the line where r is closest to pc's R 
 
-        //set steps for Radius line
-        fDistanceToPrevious[idxPos+i*n1]=fC1[idxPos+i*n1]-fC1[idxPos+i*n1-1];
-        fDistanceToNext[idxPos+i*n1]=fC1[idxPos+i*n1+1]-fC1[idxPos+i*n1];
+      //set steps for Radius line
+      fDistanceToPrevious[idxPos+i*n1]=fC1[idxPos+i*n1]-fC1[idxPos+i*n1-1];
+      fDistanceToNext[idxPos+i*n1]=fC1[idxPos+i*n1+1]-fC1[idxPos+i*n1];
 
-        //set steps for two line aside the previous line
-        fDistanceToPrevious[idxPos+i*n1+1]=fDistanceToNext[idxPos+i*n1];
-        fDistanceToNext[idxPos+i*n1-1]=fDistanceToPrevious[idxPos+i*n1];
-    }
-    int idxNeg=n1-idxPos-1;
-    cout<<idxNeg<<" "<<fC1[idxNeg]<<" "<<fC2[idxNeg]<<endl;
-    for(int i=0;i<n2;i++) {
-        fC1[idxNeg+i*n1]=-PointR;//set r for the line where r is closest to pc's R 
+      //set steps for two line aside the previous line
+      fDistanceToPrevious[idxPos+i*n1+1]=fDistanceToNext[idxPos+i*n1];
+      fDistanceToNext[idxPos+i*n1-1]=fDistanceToPrevious[idxPos+i*n1];
+   }
+   int idxNeg=n1-idxPos-1;
+   cout<<idxNeg<<" "<<fC1[idxNeg]<<" "<<fC2[idxNeg]<<endl;
+   for(int i=0;i<n2;i++) {
+      fC1[idxNeg+i*n1]=-PointR;//set r for the line where r is closest to pc's R 
 
-        //set steps for Radius line
-        fDistanceToPrevious[idxNeg+i*n1]=fC1[idxNeg+i*n1]-fC1[idxNeg+i*n1-1];
-        fDistanceToNext[idxNeg+i*n1]=fC1[idxNeg+i*n1+1]-fC1[idxNeg+i*n1];
+      //set steps for Radius line
+      fDistanceToPrevious[idxNeg+i*n1]=fC1[idxNeg+i*n1]-fC1[idxNeg+i*n1-1];
+      fDistanceToNext[idxNeg+i*n1]=fC1[idxNeg+i*n1+1]-fC1[idxNeg+i*n1];
 
-        //set steps for two line aside the previous line
-        fDistanceToPrevious[idxNeg+i*n1+1]=fDistanceToNext[idxNeg+i*n1];
-        fDistanceToNext[idxNeg+i*n1-1]=fDistanceToPrevious[idxNeg+i*n1];
-    }
+      //set steps for two line aside the previous line
+      fDistanceToPrevious[idxNeg+i*n1+1]=fDistanceToNext[idxNeg+i*n1];
+      fDistanceToNext[idxNeg+i*n1-1]=fDistanceToPrevious[idxNeg+i*n1];
+   }
 
 }
 void PointContactRZ::Initialize()
@@ -61,7 +62,7 @@ void PointContactRZ::Initialize()
       Error("Initialize", "Number of grids in R cannot be odd, abort!");
       abort();
    }
-            
+
    // The step length is calculated with the following equation:
    // BEGIN_HTML
    // <pre>
