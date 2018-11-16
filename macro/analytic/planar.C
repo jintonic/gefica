d4e4896baa8ca1a5fc12494c7c4854b1d8ee5e2e
Jing LIU <jing.liu@usd.edu>
2018-11-15 23:03:58 -0600

    minor changes

--- macro/analytic/planar.C
+++ macro/analytic/planar.C
@@ -6,7 +6,7 @@ static const double C=1; // Coulomb
 static const double e=1.6e-19*C; // elementary charge
 static const double epsilon0=8.854187817e-14*C/volt/cm; // vacuum permittivity
 // https://link.springer.com/chapter/10.1007/10832182_519
-static const double epsilon=15.8; // Ge dielectric constant
+static const double epsilonGe=15.8; // Ge dielectric constant
 //______________________________________________________________________________
 // V"(x)=a, https://www.wolframalpha.com/input/?i=V%27%27(x)%3Da
 double V(double *coordinates, double *parameters)
@@ -18,7 +18,7 @@ double V(double *coordinates, double *parameters)
    double v1= parameters[3]; // upper voltage
    double rho=parameters[4]; // space charge density [C/cm3]
 
-   double a =-rho/epsilon0/epsilon;
+   double a =-rho/epsilon0/epsilonGe;
    double c2= (v1-v0)/(x1-x0) - a/2*(x1+x0);
    double c1= (v0*x1-v1*x0)/(x1-x0) + a/2*x0*x1;
    return a*x*x/2 + c2*x + c1;
@@ -34,7 +34,7 @@ double E(double *coordinates, double *parameters)
    double v1= parameters[3];
    double rho=parameters[4];
 
-   double a =-rho/epsilon0/epsilon;
+   double a =-rho/epsilon0/epsilonGe;
    double c2= (v1-v0)/(x1-x0) - a/2*(x1+x0);
    return -a*x - c2;
 }
@@ -58,15 +58,11 @@ void drawV()
       fV[i]->SetLineStyle(i+1);
       fV[i]->SetLineColor(i+1);
       if (i+1==5) fV[i]->SetLineColor(kMagenta); // yellow -> magenta
-      if (i==0) fV[i]->Draw();
-      else fV[i]->Draw("same");
+      if (i==0) fV[i]->Draw(); else fV[i]->Draw("same");
       // net impurity concentration = - rho/e
       l->AddEntry(fV[i],Form("%8.1e",-rho[i]/e*cm3),"l");
    }
-   fV[0]->SetTitle("");
-   fV[0]->GetXaxis()->SetTitle("Thickness [cm]");
-   fV[0]->GetYaxis()->SetTitle("Voltage [V]");
-
+   fV[0]->SetTitle(";Thickness [cm];Voltage [V]");
    l->Draw();
    gPad->Print("Vx.png");
 }
@@ -88,15 +84,11 @@ void drawE()
       fE[i]->SetLineStyle(i+1);
       fE[i]->SetLineColor(i+1);
       if (i+1==5) fE[i]->SetLineColor(kMagenta); // yellow -> magenta
-      if (i==0) fE[i]->Draw();
-      else fE[i]->Draw("same");
+      if (i==0) fE[i]->Draw(); else fE[i]->Draw("same");
       // net impurity concentration = - rho/e
       l->AddEntry(fE[i],Form("%8.1e",-rho[i]/e*cm3),"l");
    }
-   fE[0]->SetTitle("");
-   fE[0]->GetXaxis()->SetTitle("Thickness [cm]");
-   fE[0]->GetYaxis()->SetTitle("Electric field [V/cm]");
-
+   fE[0]->SetTitle(";Thickness [cm];Electric field [V/cm]");
    l->Draw();
    c->Print("Ex.png");
 }
