9c9b4258f88cfa0e3621dae14c62143209877fbc
byljcron <byron.li@usd.local@login.cluster>
2018-12-17 15:23:19 -0600

    equation currected

--- examples/analytic/CV.C
+++ examples/analytic/CV.C
@@ -9,24 +9,31 @@ static const double e=1.6e-19*C; // elementary charge
 static const double epsilon0=8.854187817e-14*C/volt/cm; // vacuum permittivity
 // https://link.springer.com/chapter/10.1007/10832182_519
 static const double epsilonGe=15.8; // Ge dielectric constant
-double VtoC(double V)
+double VtoC(double V,double totalD)
 {
    double impurity=-1e10*e/cm3;
-   double d=sqrt(-2*epsilonGe*epsilon0*V/impurity+1)-1;
+   double d=sqrt(-2*epsilonGe*epsilon0*V/impurity);
+   //V=ax^2+c2x+c1
+   //c1=0, when V=0 at x=0
+   //c2=-ad, when E=dV/dx=0 at x=0, where just depleted
+   //a is impurity/epsilon
+   //V=-ax^2/2, solve V when x=d
+   if (d>totalD)d=totalD;
+   //when it depleted whole detector
    double Cap=epsilon0*epsilonGe/d;
    return Cap;
 
 }
 void CV()
 {
-   int n=20;
+   int n=200;
    double Cap[n],V[n];
    for (int i=1;i<=n;i++)
    {
       V[i-1]=i*10*volt;
-      Cap[i-1]=VtoC(V[i-1]);
+      Cap[i-1]=VtoC(V[i-1],1*cm);
    }
-   TGraph *gr  = new TGraph(n,Cap,V);
+   TGraph *gr  = new TGraph(n,V,Cap);
    TCanvas *c1 = new TCanvas("c1","Graph Draw Options",
                                          200,10,600,400);
 
