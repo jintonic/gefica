ea68b5883a10ee14d5578188f7199d8990e50017
Jing LIU <jing.liu@usd.edu>
2018-12-18 21:56:29 -0800

    updated

--- examples/analytic/CV.C
+++ examples/analytic/CV.C
@@ -1,37 +1,43 @@
-// create C-V curve for an ideal planar detector
 // definition of necessary units
 static const double cm=1;
 static const double cm3=cm*cm*cm;
 static const double volt=1;
-static const double C=1; // Coulomb
-static const double e=1.6e-19*C; // elementary charge
-static const double epsilon0=8.854187817e-14*C/volt/cm; // vacuum permittivity
+static const double Coulomb=1;
+static const double F=Coulomb/volt; // Farat
+static const double pF=1e-12*F;
+static const double e=1.6e-19*Coulomb; // elementary charge
+static const double epsilon0=8.854187817e-14*Coulomb/volt/cm;
 // https://link.springer.com/chapter/10.1007/10832182_519
 static const double epsilonGe=15.8; // Ge dielectric constant
-double VtoC(double V,double totalD)
+//______________________________________________________________________________
+//
+double C(double V)
 {
-   double impurity=-1e10*e/cm3;
-   double d=sqrt(-2*epsilonGe*epsilon0*V/impurity);
-   //V=ax^2+c2x+c1
-   //c1=0, when V=0 at x=0
-   //c2=-ad, when E=dV/dx=0 at x=0, where just depleted
-   //a is impurity/epsilon
-   //V=-ax^2/2, solve V when x=d
-   if (d>totalD)d=totalD;
-   //when it depleted whole detector
-   double Cap=epsilon0*epsilonGe*1*cm*cm/d;
-   return Cap*1e12;
+   // V"(x)=a, https://www.wolframalpha.com/input/?i=V%27%27(x)%3Da
+   // => V(x) = a*x*x/2 + c2*x + c1 (a=-rho/epsilon)
+   // => E(x) = -V'(x) = -a*x - c2 (a=-rho/epsilon)
+   // V(x=0)=0 => c1=0
+   // E(x=d)=0 => c2=-ad (d is the thickness of the depleted region)
+   // Hence V(d) = -ad^2/2, hence d = sqrt(-2V/a)
+   double rho=1e10*e/cm3; // space charge density
+   double a=-rho/epsilon0/epsilonGe;
+   double d=sqrt(-2*V/a);
+   double thickness=1*cm; // detector thickness
+   if (d>thickness) d=thickness;
+   double area=1*cm*1*cm; // unit area
+   return epsilon0*epsilonGe*area/d; // C = epsilon * area / d
 }
 //______________________________________________________________________________
 //
 void CV()
 {
-   const int n=101;
-   double Cap[n],V[n];
+   const int n=101; double c[n],v[n];
    for (int i=0; i<n; i++) {
-      V[i]=(i+1)*10*volt;
-      Cap[i]=VtoC(V[i],1*cm);
+      v[i]=(i+1)*10;
+      c[i]=C(v[i]*volt)/pF;
    }
+   TGraph *gr = new TGraph(n,v,c);
+
    // pick up a good default drawing style to modify
    gROOT->SetStyle("Plain");
    gStyle->SetLabelFont(132,"XY");
@@ -42,12 +48,11 @@ void CV()
    gStyle->SetPadRightMargin(0.01);
    gStyle->SetPadLeftMargin(0.08);
    gStyle->SetPadTopMargin(0.01);
+
    // draw C VS V
-   TGraph *gr  = new TGraph(n,V,Cap);
    gr->Draw("ac");
-   gr->SetTitle("");
+   gr->SetTitle(";Bias voltage [V];Capacitance [pC]");
    gr->SetLineWidth(2);
-   gr->GetXaxis()->SetTitle("Bias voltage [V]");
-   gr->GetYaxis()->SetTitle("Capacitance [pC]");
+
    gPad->Print("CV.png");
 }
--- examples/analytic/README.md
+++ examples/analytic/README.md
@@ -4,5 +4,6 @@
 - `planar.C`: analytic solution of Poisson's Eq. in Cartesian coordinates
 - `coaxial.C`: analytic solution of Poisson's Eq. in Cylindrical coordinates
 - `spherical.C`: analytic solution of Poisson's Eq. in Spherical coordinates
+- `CV.C`: create C-V curve for an ideal planar detector
 
 The impurity concentration is assumed to be constant throughout the volume.
