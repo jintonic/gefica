5dc774b3c26939983fcf0bb58ace8ace6afd8c23
luren <byljcron@gmail.com>
2018-12-31 16:08:21 -0600

    add header

--- examples/pointContact/r2t.C
+++ examples/pointContact/r2t.C
@@ -4,8 +4,23 @@
 
    ifstream infile("ev.new");
    ofstream outfile("result.txt");
-   double x,y,v,er,ez,e,anotherV,E1,E2,E;
+
+   double x,y,v,er,ez,e,anotherV,E1,E2,E,sizeofr,sizeofz;
    while (infile>>x>>y>>v>>e>>er>>ez) {
+      sizeofr=x;
+      sizeofz=y;
+   }
+   outfile<<"# radius of detector: "<<sizeofr<<"\n";
+   outfile<<"# height of detector: "<<sizeofz<<"\n";
+   outfile<<"# number of grid on r: "<<n1<<"\n";
+   outfile<<"# number of grid on z: "<<n2<<"\n";
+   outfile<<"# voltage of one end: "<<detector2->V0<<"\n";
+   outfile<<"# voltage of other end: "<<detector2->V1<<"\n";
+   outfile<<"# r z V(gefica) dV Er(V/cm) Ez(V/cm) E(V/cm) dEr dEz dE "<<sizeofz<<"\n";
+   outfile.seekg(0, std::ios::beg); 
+   while (infile>>x>>y>>v>>e>>er>>ez) {
+      sizeofr=x;
+      sizeofz=y;
       anotherV=detector2->GetPotential(x/10,y/10);
       E1=detector2->GetE1(x/10,y/10);
       E2=detector2->GetE2(x/10,y/10);
