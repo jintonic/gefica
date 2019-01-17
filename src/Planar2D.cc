715abc6b21b083b1663fd728486e2613853fcc0b
Jing LIU <jing.liu@usd.edu>
2019-01-17 15:03:16 -0600

    fixed bug in initialization

--- src/Planar2D.cc
+++ src/Planar2D.cc
@@ -14,6 +14,7 @@ void Planar2D::Initialize()
    XY::SetStepLength(steplength1,steplength2);
    for (int i=n; i-->0;) fC1[i]=fC1[i]+XLowerBound;
    for (int i=n; i-->0;) fC2[i]=fC2[i]+YLowerBound;
+   for (int i=n; i-->0;) fPotential[i]=V0;
    for(int i=0;i<n2;i++) {
       fIsFixed[i*n1]=true;
       fIsFixed[(i+1)*n1-1]=true;
