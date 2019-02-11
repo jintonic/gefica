8df61fa22777cbfecd2f53c3a0a950caa2fec202
Jing LIU <jing.liu@usd.edu>
2019-02-11 17:06:31 -0600

    updated

--- examples/depletion/voltage.C
+++ examples/depletion/voltage.C
@@ -1,99 +1,97 @@
+// binary search for the depletion voltage of a point contact detector
+using namespace GeFiCa;
+void voltage()
 {
-   GeFiCa::PointContactRZ *wholedetector = new GeFiCa::PointContactRZ(690,505);
-   wholedetector->Radius=3.45*GeFiCa::cm;
-   wholedetector->ZUpperBound=5.05*GeFiCa::cm;
-   wholedetector->PointR=0.14*GeFiCa::cm;
-   wholedetector->PointDepth=0.21*GeFiCa::cm;
-   wholedetector->ContactInnerR=1.450;
+   // potentials from both impurity and bias
+   PointContactDZ *totalPotential = new PointContactDZ(690,505);
 
-   wholedetector->MaxIterations=1e5;
-   wholedetector->Precision=1e-7*GeFiCa::volt;
-   wholedetector->Csor=1.994;
-   wholedetector->V0=2500*GeFiCa::volt;
-   wholedetector->V1=0*GeFiCa::volt;
+   totalPotential->V0=2500*volt;
+   totalPotential->V1=0*volt;
 
-   TF2 *im=new TF2("f","-0.318e10+0.025e10*y");
-   wholedetector->SetImpurity(im);
-   //wholedetector->Impurity="-0.318e10+0.025e10*y";//-0.01e10/GeFiCa::cm3);
+   totalPotential->Z=5.05*cm;
+   totalPotential->Radius=3.45*cm;
+   totalPotential->PointContactZ=0.21*cm;
+   totalPotential->PointContactR=0.14*cm;
 
+   totalPotential->Csor=1.994;
+   totalPotential->MaxIterations=1e5;
+   totalPotential->Precision=1e-7*volt;
 
-   GeFiCa::PointContactRZ *justimpurity = new GeFiCa::PointContactRZ(690,505);
-   justimpurity->Radius=3.45;
-   justimpurity->ZUpperBound=5.05;
-   justimpurity->PointR=0.14;
-   justimpurity->PointDepth=0.21;
-   justimpurity->ContactInnerR=1.450;
+   TF3 *impurityDistribution = new TF3("fi","-0.318e10+0.025e10*y");
+   totalPotential->SetImpurity(impurityDistribution);
 
-   justimpurity->MaxIterations=1e5;
-   justimpurity->Precision=1e-7;
-   justimpurity->Csor=1.994;
+   // potential due to space charge alone
+   PointContactDZ *impurityPotential = new PointContactDZ(690,505);
 
-   TF2 *im2=new TF2("f","-0.318e10+0.025e10*y");
-   justimpurity->SetImpurity(im2);
-   //justimpurity->Impurity="-0.318e10+0.025e10*y";//-0.01e10/GeFiCa::cm3);
+   impurityPotential->V0=0; // no bias
+   impurityPotential->V1=0; // no bias
 
+   impurityPotential->Z=5.05*cm;
+   impurityPotential->Radius=3.45*cm;
+   impurityPotential->PointContactZ=0.21*cm;
+   impurityPotential->PointContactR=0.14*cm;
 
-   justimpurity->V1=0;
-   justimpurity->V0=0;
-   justimpurity->CalculatePotential(GeFiCa::kSOR2);
-   justimpurity->SaveField("im");
-   justimpurity->LoadField("im");
+   impurityPotential->Csor=1.994;
+   impurityPotential->MaxIterations=1e5;
+   impurityPotential->Precision=1e-7*volt;
 
-   GeFiCa::PointContactRZ *weightingPotential = new GeFiCa::PointContactRZ(690,505);
-   weightingPotential->Radius=3.45*GeFiCa::cm;
-   weightingPotential->ZUpperBound=5.05*GeFiCa::cm;
-   weightingPotential->PointR=0.14*GeFiCa::cm;
-   weightingPotential->PointDepth=0.21*GeFiCa::cm;
-   weightingPotential->ContactInnerR=1.450;
+   impurityPotential->SetImpurity(impurityDistribution);
 
-   weightingPotential->MaxIterations=1e5;
-   weightingPotential->Precision=1e-7*GeFiCa::volt;
+   impurityPotential->CalculatePotential(kSOR2);
+   //impurityPotential->SaveField("im");
+   //impurityPotential->LoadField("im");
+
+   // weighting potential
+   PointContactDZ *weightingPotential = new PointContactDZ(690,505);
+
+   weightingPotential->V0=1*volt;
+   weightingPotential->V1=0*volt;
+
+   weightingPotential->Z=5.05*cm;
+   weightingPotential->Radius=3.45*cm;
+   weightingPotential->PointContactZ=0.21*cm;
+   weightingPotential->PointContactR=0.14*cm;
+
    weightingPotential->Csor=1.994;
-   weightingPotential->V0=1*GeFiCa::volt;
-   weightingPotential->V1=0*GeFiCa::volt;
+   weightingPotential->MaxIterations=1e5;
+   weightingPotential->Precision=1e-7*volt;
 
+   weightingPotential->SetImpurity(new TF3("wpi","0"));
 
-   TF2 *im3=new TF2("f","0.0+0.*y");
-   weightingPotential->SetImpurity(im3);
-   weightingPotential->CalculatePotential(GeFiCa::kSOR2);
-   weightingPotential->SaveField("wp");
-   weightingPotential->LoadField("wp");
-   weightingPotential->CalculateCapacitance();
-   wholedetector = (GeFiCa::X*) weightingPotential.Clone("newX")
-   //wholedetector->LoadField("wp");
-   (*wholedetector) *= (double)2;
-   GeFiCa::PointContactRZ * multiweightPotential=new GeFiCa::PointContactRZ(100,100);
-   multiweightPotential = (GeFiCa::X*) weightingPotential.Clone("newX")
-   // multiweightPotential  ->LoadField("wp");
-   (*wholedetector) += justimpurity;
-   wholedetector->SaveField("beforejump");
+   weightingPotential->CalculatePotential(kSOR2);
+   //weightingPotential->SaveField("wp");
+   //weightingPotential->LoadField("wp");
+
+   totalPotential = (PointContactDZ*) weightingPotential->Clone("total");
+   (*totalPotential) *= 2.;
+
+   PointContactDZ *multiweightPotential = new PointContactDZ(100,100);
+   multiweightPotential = (PointContactDZ*) weightingPotential->Clone("total");
+
+   (*totalPotential) += impurityPotential;
+   totalPotential->SaveField("beforejump");
    int stepsize=2;
-   while(!wholedetector->IsDepleted())
-   {
+   while(!totalPotential->IsDepleted()) {
       (*multiweightPotential)*=(double)2;
-      (*wholedetector)+=multiweightPotential;
+      (*totalPotential)+=multiweightPotential;
       stepsize=stepsize*2;
    }
    double upper=stepsize;
    std::cout<<stepsize;
    double lower=0;
    double mid=upper/2;
-   while(lower<=upper)
-   {
+   while(lower<=upper) {
       mid=(upper+lower)/2;
-      wholedetector = (GeFiCa::X*) weightingPotential.Clone("newX")
-      //wholedetector->LoadField("wp");
-      (*wholedetector)*=mid;
-      (*wholedetector)+=justimpurity;
-      if(wholedetector->IsDepleted())
-      {
+      totalPotential = (PointContactDZ*) weightingPotential->Clone("total");
+      //totalPotential->LoadField("wp");
+      (*totalPotential)*=mid;
+      (*totalPotential)+=impurityPotential;
+      if(totalPotential->IsDepleted()) {
          upper=mid-1e-5;
-      }
-      else
-      {
+      } else {
          lower=mid+1e-5;
       }
    }
-   wholedetector->SaveField("result.root");
+   totalPotential->SaveField("result.root");
 }
 
