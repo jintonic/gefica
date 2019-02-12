b6ec0d48c1d950190d797ea1f1a5b92dd05f378f
Jing LIU <jing.liu@usd.edu>
2019-02-12 17:38:06 -0600

    added default numbers

--- src/PointContactDZ.h
+++ src/PointContactDZ.h
@@ -10,7 +10,7 @@ class GeFiCa::PointContactDZ : public GeFiCa::RZ
    public:
       double Radius,Z,PointContactR,PointContactZ,WrapArroundR,TaperLength,TaperZ;//bounds for X and Y and point start and end
  
-      PointContactDZ(int ix, int iy) : RZ(ix, iy),
+      PointContactDZ(int ix=101, int iy=101) : RZ(ix, iy),
       Radius(1),Z(1), PointContactR(0.4),PointContactZ(0.2),WrapArroundR(1),TaperLength(0),TaperZ(0),Z0(0){};
 
       /**
