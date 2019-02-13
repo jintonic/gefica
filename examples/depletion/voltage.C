9202e9d17f107fd112c0a4f83b14a551a274be0d
Jing LIU <jing.liu@usd.edu>
2019-02-13 17:08:34 -0600

    minor improvement

--- examples/depletion/voltage.C
+++ examples/depletion/voltage.C
@@ -2,59 +2,55 @@
 using namespace GeFiCa;
 void voltage()
 {
-   TFile *fi, *fw;
-   PointContactDZ *vi, *vw, *vt;
+   PointContactDZ *vi; // potential (v) due to impurity (i) alone
+   PointContactDZ *vw; // weighting (w) potential (v)
+   PointContactDZ *vt; // totoal (t) potential (v) = vw * bias + vi
+   TFile *fi; // ROOT file used to save vi
+   TFile *fw; // ROOT file used to save vw
 
-   // potential due to space charge alone
-   if (FILE *input = fopen("impurity.root","r")) { // if already calculated
+   cout<<"\ncalculate or load potential due to space charge alone:"<<endl;
+   if (FILE *input = fopen("impurity.root","r")) { // load from fi
       fclose(input);
       fi = new TFile("impurity.root"); // read only
       vi = (PointContactDZ*) fi->Get("vi");
-   } else { // if not calculated before
+   } else { // a fresh calculation
       fi = new TFile("impurity.root", "recreate");
 
-      vi = new PointContactDZ(690,505);
-      vi->SetName("vi");
+      vi = new PointContactDZ(690,505); vi->SetName("vi");
 
-      vi->V0=0; // no bias
-      vi->V1=0; // no bias
+      vi->V0=0; vi->V1=0; // no bias
 
-      vi->Z=5.05*cm;
-      vi->Radius=3.45*cm;
-      vi->PointContactZ=0.21*cm;
-      vi->PointContactR=0.14*cm;
+      vi->Z=5.05*cm; vi->Radius=3.45*cm;
+      vi->PointContactZ=0.21*cm; vi->PointContactR=0.14*cm;
 
       vi->Csor=1.994;
       vi->MaxIterations=1e5;
       vi->Precision=1e-7*volt;
 
+      // x in TF3 -> r in PointContactDZ, y in TF3 -> z in PointContactDZ
       TF3 *impurityDistribution = new TF3("fi","-0.318e10+0.025e10*y");
       vi->SetImpurity(impurityDistribution);
 
       vi->CalculatePotential(kSOR2);
 
-      vi->Write();
+      vi->Write(); // save itself to fi
    }
-   vi->Dump();
+   vi->Dump(); // print configurations
 
-   // weighting potential
-   if (FILE *input = fopen("weighting.root","r")) { // if already calculated
+   cout<<"\ncalculate or load weighting potential:"<<endl;
+   if (FILE *input = fopen("weighting.root","r")) { // load from fw
       fclose(input);
       fw = new TFile("weighting.root"); // read only
       vw = (PointContactDZ*) fw->Get("vw");
-   } else { // if not calculated before
+   } else { // a fresh calculation
       fw = new TFile("weighting.root", "recreate");
 
-      vw = new PointContactDZ(690,505);
-      vw->SetName("vw");
+      vw = new PointContactDZ(690,505); vw->SetName("vw");
 
-      vw->V0=1*volt;
-      vw->V1=0*volt;
+      vw->V0=1*volt; vw->V1=0*volt; // weighting potential
 
-      vw->Z=5.05*cm;
-      vw->Radius=3.45*cm;
-      vw->PointContactZ=0.21*cm;
-      vw->PointContactR=0.14*cm;
+      vw->Z=5.05*cm; vw->Radius=3.45*cm;
+      vw->PointContactZ=0.21*cm; vw->PointContactR=0.14*cm;
 
       vw->Csor=1.994;
       vw->MaxIterations=1e5;
@@ -64,22 +60,26 @@ void voltage()
 
       vw->CalculatePotential(kSOR2);
 
-      vw->Write();
+      vw->Write(); // save itself to fw
    }
-   vw->Dump();
+   vw->Dump(); // print configurations
 
-   double bias, vlower=0*volt, vupper=2e4*volt; // range of search
-   while (vupper-vlower>1e-3*volt) { // binary search
-      bias=(vupper+vlower)/2; // bias voltage
+   double bias, vLower=0*volt, vUpper=2e4*volt; // range of search
+   cout<<"\nStart binary search in ["<<vLower<<", "<<vUpper<<"] V"<<endl;
+   while (vUpper-vLower>0.1*volt) { // binary search
+      bias=(vUpper+vLower)/2; // a new guess
+      // vt = vw*bias + vi
       vt = (PointContactDZ*) vw->Clone("vt");
-      (*vt)*=bias;
-      (*vt)+=vi;
-      if (!vt->IsDepleted()) vlower=bias;
-      else vupper=bias;
+      (*vt)*=bias; (*vt)+=vi;
+      // updating range
+      if (vt->IsDepleted()) vUpper=bias;
+      else vLower=bias;
+
       delete vt;
-      cout<<"bias: "<<bias<<", u: "<<vupper<<", l: "<<vlower<<endl;
+      cout<<"Current guess: "<<bias<<" V, ";
+      cout<<"new search range: ["<<vLower<<", "<<vUpper<<"]"<<endl;
    }
-   cout<<"depletion voltage: "<<bias<<endl;
+   cout<<"The depletion voltage is found to be: "<<bias<<" V"<<endl;
 
    fw->Close();
    fi->Close();
