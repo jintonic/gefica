ed553b9bcdbe3612574ecd172e9a9a8e9c554bc0
Jing LIU <jing.liu@usd.edu>
2019-02-28 09:10:32 -0600

    updated

--- examples/planar/showConvergingSteps.cc
+++ examples/planar/showConvergingSteps.cc
@@ -8,13 +8,13 @@ void showConvergingSteps()
    detector->V1=300*volt;
    detector->V0=0*volt;
 
-   int n=3;
    TMultiGraph *mg = new TMultiGraph;
+   const int n=4;
    for (int i=0;i<n;i++) {
       detector->MaxIterations=i*50;
       detector->CalculatePotential(GeFiCa::kSOR2);
-      TTree *t = detector->GetTree();
-      t->Draw("v:c1");
+      TTree *t = detector->GetTree(true);
+      t->Draw("v:c1","","goff");
       TGraph *tg = new TGraph(t->GetSelectedRows(), t->GetV2(), t->GetV1());
       tg->SetMarkerColor(i+2);
       tg->SetMarkerStyle(6);
