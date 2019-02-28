bed24490c4f4870a88e2a3bf245c624090dffb85
Jing LIU <jing.liu@usd.edu>
2019-02-28 07:17:08 -0600

    updated

--- examples/planar/showConvergingSteps.cc
+++ examples/planar/showConvergingSteps.cc
@@ -1,35 +1,26 @@
+// Show intermediate steps of a converging process
 using namespace GeFiCa;
-/**
- * \file showConvergingSteps.cc
- * \example planar/showConvergingSteps.cc
- * \brief Show middle steps of the converging process.
- */
+void showConvergingSteps()
 {
-   // calculate fields
-   GeFiCa::Planar1D *detector = new GeFiCa::Planar1D(101);
-   detector->MaxIterations=1;
-   detector->Csor=1.9;
-   detector->UpperBound=1.175*GeFiCa::cm;
-   detector->V1=300*GeFiCa::volt;
-   detector->V0=0*GeFiCa::volt;
-   detector->SetAverageImpurity(1e10/GeFiCa::cm3);
+   Planar1D *detector = new GeFiCa::Planar1D;
+   detector->SetAverageImpurity(1e10/cm3);
+   detector->Thickness=1*cm;
+   detector->V1=300*volt;
+   detector->V0=0*volt;
 
-   int n=10;
-      TCanvas *C = new TCanvas();
-      TMultiGraph *mg = new TMultiGraph();
-   for (int i=0-1;i<n;i++) {
-      detector->MaxIterations=i*20;
+   int n=3;
+   TMultiGraph *mg = new TMultiGraph;
+   for (int i=0;i<n;i++) {
+      detector->MaxIterations=i*50;
       detector->CalculatePotential(GeFiCa::kSOR2);
-      detector->SaveField("planar1dSOR2.root");
-      TChain *tn=new TChain("t");
-      tn->Add("planar1dSOR2.root");
-      tn->Draw("v:c1");
-      TGraph *tg=new TGraph(tn->GetSelectedRows(), tn->GetV2(), tn->GetV1());
+      TTree *t = detector->GetTree();
+      t->Draw("v:c1");
+      TGraph *tg = new TGraph(t->GetSelectedRows(), t->GetV2(), t->GetV1());
       tg->SetMarkerColor(i+2);
       tg->SetMarkerStyle(6);
       mg->Add(tg);
    }
    mg->SetTitle("; Thickness [cm]; Potential [V]");
-   
+
    mg->Draw("ap");
 }
