1e7059439d48441ef5efb8d4820c1e45f2aaf9a6
Jing LIU <jing.liu@usd.edu>
2019-03-01 09:57:13 -0600

    added mm, kV

--- src/Units.h
+++ src/Units.h
@@ -5,13 +5,18 @@
 #define GeFiCa_UNITS_H
 
 namespace GeFiCa { 
-   static const double C=1;
-   static const double cm=1;
-   static const double cm3=cm*cm*cm;
+   static const double C=1; ///< charge unit Coulomb
+   static const double cm=1; ///< centimeter
+   static const double cm3=cm*cm*cm; ///< centimeter cubed
+   static const double mm=0.1*cm; ///< minimeter
+   static const double mm3=mm*mm*mm; ///< minimeter cubed
    static const double volt=1;
-   static const double pF=C/volt*1e-12;
+   static const double kV=1000*volt; ///< kilo volt
+   static const double pF=C/volt*1e-12; ///< pico farad
    static const double Qe=1.6e-19*C;  ///< electron charge in Coulomb [C]
-   static const double epsilon=16*8.854187817e-14*C/volt/cm; ///< dielectric constant of Ge [C/volt/cm]
+   static const double epsilon0=8.854187817e-14*C/volt/cm; ///< vacumm permittivity [C/volt/cm]
+   static const double epsilonR=16; ///< dielectric constant of Ge
+   static const double epsilon=epsilonR*epsilon0; ///< permittivity of Ge [C/volt/cm]
 }
 
 #endif
