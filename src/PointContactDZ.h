0a163839b8080b243d1f8adbfdbea6c06b678b98
Jing LIU <jing.liu@usd.edu>
2019-03-01 06:56:19 -0600

    change n1 from 101 to 100

--- src/PointContactDZ.h
+++ src/PointContactDZ.h
@@ -7,7 +7,7 @@ namespace GeFiCa { class PointContactDZ; }
 
 /**
  * Grid setup for 2D point contact detectors.
- * The grid is setup in [-Radius, Radius] and [0, Z0].
+ * The grid is setup in [-Radius, Radius] and [0, Z].
  */
 class GeFiCa::PointContactDZ : public GeFiCa::RhoZ
 {
@@ -17,7 +17,7 @@ class GeFiCa::PointContactDZ : public GeFiCa::RhoZ
       double Radius,Z,PointContactR,PointContactZ,WrapArroundR,TaperLength,TaperZ;//bounds for X and Y and point start and end
  
      
-      PointContactDZ(int ix=101, int iy=101) : RhoZ(ix, iy),
+      PointContactDZ(int ix=100, int iy=101) : RhoZ(ix, iy),
       Z0(0),  HoleInnerR(0.3), HoleOutterR(0.5),
       ConnorLength(0.2), ConnorZ(0.3), HoleZ(0.2), 
       Radius(1),Z(1), PointContactR(0.4),PointContactZ(0.2),WrapArroundR(1),TaperLength(0.1),TaperZ(0.1){};
