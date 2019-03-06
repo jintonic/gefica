f23606e8efc4d54fa8bc815905d78539298db866
Jing LIU <jing.liu@usd.edu>
2019-03-06 10:59:13 -0600

    updated

--- examples/planar/verifyCV.cc
+++ examples/planar/verifyCV.cc
@@ -1,116 +1,95 @@
+// Compare numerically calculated C-V curves with analytic solution
 using namespace GeFiCa;
-// Compare numerically calculated C-V curve with analytic calculation
-// Since C=epsilon*A/d, C-V curve is basically 1/d-V curve
-double GetDepthOfDepletionNumerically(double voltage, double thickness)
+const int n=2001;
+// get capacitance based on C=epsilon*A/d
+double GetCfromDepletionDepth(double voltage, double thickness)
 {
-   const int n=2001;
    // calculate fields
-   Planar1D *detector = new Planar1D(n);
-   detector->Thickness=thickness*cm;
-   detector->V1=0*volt;
-   detector->V0=voltage*volt;
+   Planar1D detector(n);
+   detector.Thickness=thickness;
+   detector.V0=voltage; // for bottom electrode
+   detector.V1=0*volt; // for top electrode
 
-   TF3 *im=new TF3("f","-1e10");
-   detector->SetImpurity(im);
-   bool *connect=new bool[n];
-   for(int i=0;i<n;i++) {
-      connect[i]=false;
-   }
-   connect[0]=true;
-
-   detector->CalculatePotential(kSOR2);
+   TF3 *fi = new TF3("f","-1e10"); // 1/cm3
+   detector.SetImpurity(fi);
+   detector.Csor=1.994;
+   detector.MaxIterations=10000;
+   detector.CalculatePotential(kSOR2);
+   delete fi;
    
-   //search for first V0
-   double begin=0;
-   double end=thickness;
-   double mid=(begin+end)/2;
-   while(begin<end)
-   {
-      mid=(begin+end)/2;
-      if(detector->GetV(mid)>0)
-      {
-         begin=mid+1e-5;
-      }
-      if(detector->GetV(mid)<=0)
-      {
-         end=mid-1e-5;
-      }
+   //search for depletion depth
+   double up=thickness, down=0, depth=thickness;
+   while(up-down>1e-3) {
+      depth=(down+up)/2;
+      if(detector.GetV(depth)>0) down=depth;
+      if(detector.GetV(depth)<=0) up=depth;
    }
 
-   std::cout<<mid<<"\n";
-   return mid;
+   double A = 1*cm*1*cm;
+   return epsilon*A/depth;
 }
-double GetDepthOfDepletionbuildinNumerically(double voltage, double thickness)
+//______________________________________________________________________________
+// use GefiCa::X::GetC()
+double GetCfromGeFiCa(double voltage, double thickness)
 {
-   const int n=2001;
    // calculate fields
-   Planar1D *detector = new Planar1D(n);
-   detector->Thickness=thickness*cm;
-   detector->V1=0*volt;
-   detector->V0=voltage*volt;
+   Planar1D detector(n);
+   detector.Thickness=thickness;
+   detector.V0=voltage; // for bottom electrode
+   detector.V1=0*volt; // for top electrode
 
-   TF3 *im=new TF3("f","-1e10");
-   detector->SetImpurity(im);
-   detector->CalculatePotential(kSOR2);
+   TF3 *fi = new TF3("f","-1e10"); // 1/cm3
+   detector.SetImpurity(fi);
+   detector.Csor=1.994;
+   detector.MaxIterations=10000;
+   double c=detector.GetC();
+   delete fi;
    
-   return detector->GetC();
+   return c;
 }
 //______________________________________________________________________________
 //
-double GetDepthOfDepletionAnalytically(double voltage, double thickness)
+double GetCanalytically(double voltage, double thickness)
 {
-   double impurity=-1e10*Qe/cm3;
-   double d=TMath::Sqrt(-2*epsilon*voltage/impurity);
-   if (d>thickness)return thickness;
    //voltage=ax^2+c2x+c1
    //c1=0, when voltage=0 at x=0
    //c2=-ad, when E=dV/dx=0 at x=0, where just depleted
-   //a is impurity/epsilon
-   //voltage=-ax^2/2, solve voltage when x=d
-   return d;
+   //a is rho/epsilon
+   //voltage=-ax^2/2, solve voltage when x=depth
+   double rho=-1e10/cm3*Qe;
+   double depth=TMath::Sqrt(-2*epsilon*voltage/rho);
+   if (depth>thickness) depth=thickness;
+
+   double A = 1*cm*1*cm;
+   return epsilon*A/depth;
 }
 //______________________________________________________________________________
 //
 void verifyCV()
 {
-   // please don't draw diff, instead, overlay two curves on top of each other
    double thickness=1*cm;
-   const int n=15;
-   Double_t V[n], Cn[n], Ca[n],Cnb[n];
-   for (Int_t i=0;i<n;i++) {
-      V[i] = (i+1)*50;
-      Cn[i] = 1/GetDepthOfDepletionNumerically(V[i],thickness);
-      Cnb[i] = GetDepthOfDepletionbuildinNumerically(V[i],thickness)/pF;
-      Ca[i] = 1/GetDepthOfDepletionAnalytically(V[i],thickness); 
+   const int np=16;
+   Double_t V[np], Cn[np], Ca[np],Cg[np];
+   for (Int_t i=0;i<np;i++) {
+      V[i] = (i+1)*50*volt; Printf("voltage: %.0f V", V[i]);
+      Cn[i] = GetCfromDepletionDepth(V[i],thickness)/pF;
+      Cg[i] = GetCfromGeFiCa(V[i],thickness)/pF;
+      Ca[i] = GetCanalytically(V[i],thickness)/pF; 
    }
-   for(int i=0;i<n;i++)
-   {
-      Cnb[i]=Cnb[i]/Cnb[n-1];
-   }
-   // prepare drawing style
-   gROOT->SetStyle("Plain"); // pick up a good drawing style to modify
-   gStyle->SetLegendBorderSize(0);
-   gStyle->SetLegendFont(132);
-   gStyle->SetLabelFont(132,"XY");
-   gStyle->SetTitleFont(132,"XY");
-   gStyle->SetLabelSize(0.05,"XY");
-   gStyle->SetTitleSize(0.05,"XY");
+
+   TGraph *gn = new TGraph(np,V,Cn); gn->SetMarkerStyle(22);
+   TGraph *gg = new TGraph(np,V,Cg); gg->SetMarkerStyle(24);
+   TGraph *ga = new TGraph(np,V,Ca); ga->SetMarkerStyle(25);
+
+   gStyle->SetPadLeftMargin(0.07);
    gStyle->SetPadRightMargin(0.01);
-   gStyle->SetPadLeftMargin(0.12);
-   gStyle->SetPadTopMargin(0.02);
-   TGraph *gn = new TGraph(n,V,Cn);
-   TGraph *gnb = new TGraph(n,V,Cnb);
-   TGraph *ga = new TGraph(n,V,Ca);
-   gn->SetTitle(";Bias [V];Capacitance [Arbitrary Unit]");
-   gnb->SetMarkerStyle(kCircle);
-   ga->SetMarkerStyle(7);
-   //gnb->SetMarkerStyle(kCircle);
-   gnb->Draw("ap");
-   gn->Draw("*");
-   ga->Draw("l");
-   TLegend *l = new TLegend(0.5,0.6,0.8,0.8);
-   l->AddEntry(ga,"Analytic","l");
-   l->AddEntry(gn,"numerical","*");
-   l->AddEntry(gnb,"buildin numerical","p");
+   gStyle->SetTitleOffset(0.6,"y");
+   TMultiGraph *gs = new TMultiGraph; gs->Add(gn); gs->Add(gg); gs->Add(ga);
+   gs->SetTitle(";Bias [V];Capacitance [pF]"); gs->Draw("pal");
+
+   TLegend *l = new TLegend(0.5,0.6,0.99,0.97);
+   l->AddEntry(ga,"#varepsilon A/d, analytically","pl");
+   l->AddEntry(gn,"#varepsilon A/d, numerically","pl");
+   l->AddEntry(gg,"GeFiCa::X::GetC()","pl");
    l->Draw();
 }
