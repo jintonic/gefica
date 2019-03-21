b9096418e79e30e0b96c4e25bfbd438e5f9d27cd
Jing LIU <jing.liu@usd.edu>
2019-03-21 18:03:49 -0500

    combined Crystal.h Detector.h

--- src/Detector.h
+++ src/Detector.h
@@ -1,11 +1,30 @@
 #ifndef GeFiCa_Detector
 #define GeFiCa_Detector
 
+namespace GeFiCa { class Crystal; class Detector; }
+
+/**
+ * Crystal properties.
+ */
+class GeFiCa::Crystal
+{
+   public:
+      /**
+       * Height of crystal.
+       * Since the derived classes can be used to configure not only 1D but
+       * also 2D and 3D grids, this property exists in all of them.
+       */
+      double Height;
+      double TopImpurity; ///< net impurity concentration at top of crystal
+      double BottomImpurity; ///< net impurity concentration at bottom of crystal
+      /**
+       * \return net impurity concentration at \param height.
+       */
+      double GetImpurity(double height)
+      { return (TopImpurity-BottomImpurity)*height/Height+BottomImpurity; }
+};
+
 #include "Grid.h"
-
-#include "Crystal.h"
-
-namespace GeFiCa { class Detector; class Grid; }
 
 /**
  * Detector & crystal properties.
