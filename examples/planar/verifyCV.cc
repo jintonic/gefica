7f6372ac617d246de7aeaaea38aa9570485e49f7
Jing LIU <jing.liu@usd.edu>
2019-03-28 11:45:06 -0500

    updated

--- examples/planar/verifyCV.cc
+++ examples/planar/verifyCV.cc
@@ -2,23 +2,26 @@
 using namespace GeFiCa;
 const int n=401;
 // get capacitance based on C=epsilon*A/d
-double GetCfromDepletionDepth(double voltage, double thickness)
+double GetCfromDepletionDepth(double voltage, double height)
 {
-   // calculate fields
-   Planar1D detector(n);
-   detector.Thickness=thickness;
-   detector.V0=voltage; // for bottom electrode
-   detector.V1=0*volt; // for top electrode
+   Planar detector;
+   detector.Height=height;
+   detector.Bias[0]=voltage; // for bottom electrode
+   detector.Bias[1]=0*volt; // for top electrode
    detector.SetAverageImpurity(-1e10/cm3);
-   detector.RelaxationFactor=1.986;
-   detector.SuccessiveOverRelax();
+
+   X grid(n);
+   grid.GetBoundaryConditionFrom(detector);
+   grid.RelaxationFactor=1.986;
+   grid.SuccessiveOverRelax();
    
    //search for depletion depth
-   double up=thickness, down=0, depth=thickness;
+   double up=height, down=0, depth=height;
    while(up-down>1e-3) {
       depth=(down+up)/2;
-      if(detector.GetV(depth)>0) down=depth;
-      if(detector.GetV(depth)<=0) up=depth;
+      gDebug=1;
+      if(grid.GetV(depth)==0) down=depth; else up=depth;
+      gDebug=0;
    }
 
    double A = 1*cm*1*cm;
@@ -26,29 +29,32 @@ double GetCfromDepletionDepth(double voltage, double thickness)
 }
 //______________________________________________________________________________
 // use GefiCa::X::GetC()
-double GetCfromGeFiCa(double voltage, double thickness)
+double GetCfromGeFiCa(double voltage, double height)
 {
    // calculate fields
-   Planar1D detector(n);
-   detector.Thickness=thickness;
-   detector.V0=voltage; // for bottom electrode
-   detector.V1=0*volt; // for top electrode
+   Planar detector;
+   detector.Height=height;
+   detector.Bias[0]=voltage; // for bottom electrode
+   detector.Bias[1]=0*volt; // for top electrode
    detector.SetAverageImpurity(-1e10/cm3);
-   detector.RelaxationFactor=1.986;
-   return detector.GetC();
+
+   X grid(n);
+   grid.GetBoundaryConditionFrom(detector);
+   grid.RelaxationFactor=1.986;
+   return grid.GetC();
 }
 //______________________________________________________________________________
 //
-double GetCanalytically(double voltage, double thickness)
+double GetCanalytically(double voltage, double height)
 {
    //voltage=ax^2+c2x+c1
    //c1=0, when voltage=0 at x=0
    //c2=-ad, when E=dV/dx=0 at x=0, where just depleted
    //a is rho/epsilon
    //voltage=-ax^2/2, solve voltage when x=depth
-   double rho=-1e10/cm3*Qe;
-   double depth=TMath::Sqrt(-2*epsilon*voltage/rho);
-   if (depth>thickness) depth=thickness;
+   double rho=1e10/cm3*Qe;
+   double depth=TMath::Sqrt(2*epsilon*voltage/rho);
+   if (depth>height) depth=height;
 
    double A = 1*cm*1*cm;
    return epsilon*A/depth;
@@ -57,14 +63,14 @@ double GetCanalytically(double voltage, double thickness)
 //
 void verifyCV()
 {
-   double thickness=1*cm;
+   double height=1*cm;
    const int np=16;
    Double_t V[np], Cn[np], Ca[np],Cg[np];
    for (Int_t i=0;i<np;i++) {
       V[i] = (i+2)*50*volt; Printf("voltage: %.0f V", V[i]);
-      Cn[i] = GetCfromDepletionDepth(V[i],thickness)/pF;
-      Cg[i] = GetCfromGeFiCa(V[i],thickness)/pF;
-      Ca[i] = GetCanalytically(V[i],thickness)/pF; 
+      Cn[i] = GetCfromDepletionDepth(V[i],height)/pF;
+      Cg[i] = GetCfromGeFiCa(V[i],height)/pF;
+      Ca[i] = GetCanalytically(V[i],height)/pF; 
    }
 
    TGraph *gn = new TGraph(np,V,Cn); gn->SetMarkerStyle(22);
@@ -75,7 +81,7 @@ void verifyCV()
    gStyle->SetPadRightMargin(0.01);
    gStyle->SetTitleOffset(0.85,"y");
    TMultiGraph *gs = new TMultiGraph; gs->Add(gn); gs->Add(gg); gs->Add(ga);
-   gs->SetTitle(";Bias [V];Capacitance per cm^{2} [pF]"); gs->Draw("pal");
+   gs->SetTitle(";Bias [V];Capacitance per cm^{2} [pF]"); gs->Draw("pac");
 
    TLegend *l = new TLegend(0.5,0.6,0.97,0.97);
    l->AddEntry(ga,"#varepsilon A/d, analytically","pl");
