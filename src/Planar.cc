9b9da7589d9cc8456bee84cf9fba2a407d31c99b
Jing LIU <jing.liu@usd.edu>
2019-03-28 00:02:54 -0500

    added a bias voltage

--- src/Planar.cc
+++ src/Planar.cc
@@ -3,4 +3,5 @@
 using namespace GeFiCa;
 
 Planar::Planar(const char *name, const char *title)
-   : Detector(name,title), Width(1*cm), Depth(1*cm) {};
+   : Detector(name,title), Width(1*cm), Depth(1*cm)
+{ Bias.push_back(1*kV); }
