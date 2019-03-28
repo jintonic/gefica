39f30261a22e3072a603710858ab0c00b8e55d3a
Jing LIU <jing.liu@usd.edu>
2019-03-28 00:03:40 -0500

    recovered

--- src/X.cc
+++ src/X.cc
@@ -1,73 +1,99 @@
 #include "X.h"
 #include "Units.h"
-#include "Detector.h"
+#include "Planar.h"
 using namespace GeFiCa;
 
 void X::OverRelaxAt(size_t idx)
 {
-   if (fIsFixed[idx]) return;
+   if (fIsFixed[idx]) return; // no need to calculate on boundaries
 
-   double tmp = -Src[idx]*dC1m[idx]*dC1p[idx]/2 +
+   // over relax
+   double vnew = Src[idx]*dC1m[idx]*dC1p[idx]/2 +
       (dC1p[idx]*Vp[idx-1]+dC1m[idx]*Vp[idx+1])/(dC1m[idx]+dC1p[idx]);
+   vnew = RelaxationFactor*(vnew-Vp[idx]) + Vp[idx];
 
-   tmp=RelaxationFactor*(tmp-Vp[idx])+Vp[idx];
-
+   // check depletion and update Vp[idx] accordingly
    double min=Vp[idx-1], max=Vp[idx-1];
    if (min>Vp[idx+1]) min=Vp[idx+1];
    if (max<Vp[idx+1]) max=Vp[idx+1];
+   if (vnew<min) {
+      fIsDepleted[idx]=false; Vp[idx]=min;
+   } else if (vnew>max) {
+      fIsDepleted[idx]=false; Vp[idx]=max;
+   } else {
+      fIsDepleted[idx]=true; Vp[idx]=vnew;
+   }
 
-   if (tmp<min) {
-      Vp[idx]=min;
-      fIsDepleted[idx]=false;
-   } else if (tmp>max) {
-      Vp[idx]=max;
-      fIsDepleted[idx]=false;
-   } else
-      fIsDepleted[idx]=true;
+   // update Vp for impurity-only case even if the point is undepleted
+   if (fDetector->Bias[0]==fDetector->Bias[1]) Vp[idx]=vnew;
+}
+//_____________________________________________________________________________
+//
+void X::SolveAnalytically()
+{
+   if (fDetector==0) {
+      Error("SolveAnalytically", "Grid is not ready. "
+            "Please call GetBoundaryConditionFrom(Detector&) first.");
+      abort();
+   }
+   if (fDetector->TopImpurity!=fDetector->BottomImpurity) {
+      Error("SolveAnalytically", "can't handle changing impurity.");
+      abort();
+   }
+   double h=fDetector->Height;
+   double a=-Src[N1-1]/2;
+   double b=(Vp[N1-1]-Vp[0]-a*h*h)/h;
+   double c=Vp[0];
+   for (size_t i=0; i<N1; i++) Vp[i] = a*C1[i]*C1[i]+b*C1[i]+c;
+   CalculateE();
+}
+//_____________________________________________________________________________
+//
+void X::GetBoundaryConditionFrom(Detector &detector)
+{
+   if (GetN()>0) { // this function can only be called once
+      Warning("GetBoundaryConditionFrom", "has been called. Do nothing.");
+      return;
+   }
+   TString type(detector.ClassName());
+   if (type.Contains("Planar")==false) {
+      Error("GetBoundaryConditionFrom", "%s is not expected. "
+            "Please pass in a GeFiCa::Planar detector.", type.Data());
+      abort();
+   }
 
-   if(fIsDepleted[idx]||Vp.begin()==Vp.end()) Vp[idx]=tmp;
+   if (detector.Height<=0) {
+      Error("GetBoundaryConditionFrom",
+            "Height(%.1fcm)<=0, abort!", detector.Height/cm);
+      abort();
+   }
+
+   for (size_t i=0; i<N1; i++) {
+      dC1p.push_back(detector.Height/(N1-1));
+      dC1m.push_back(detector.Height/(N1-1));
+      C1.push_back(i*dC1p[i]);
+      E1.push_back(0); Et.push_back(0);
+      fIsFixed.push_back(false); fIsDepleted.push_back(false);
+      Src.push_back(-detector.GetImpurity(C1[i])*Qe/epsilon);
+   }
+   // fix 1st and last points
+   fIsFixed[0]=true; fIsFixed[N1-1]=true;
+   // linear interpolation between Bias[0] and Bias[1]
+   double slope = (detector.Bias[1]-detector.Bias[0])/(N1-1);
+   for (size_t i=0; i<N1; i++) {
+      Vp.push_back(detector.Bias[0]+slope*i);
+   }
+   Vp[N1-1]=detector.Bias[1];
+
+   fDetector = &detector; // save it for other uses
 }
 //_____________________________________________________________________________
 //
-void X::FillGridWithAnalyticResult()
+void X::CalculateE()
 {
-//   if (TopImpurity!=BottomImpurity) {
-//      Error("FillGridWithAnalyticResult",
-//            "can't handle changing impurity! Abort.");
-//      abort();
-//   }
-//   double d=Height; //Height or depth of the detector
-//   double a=-Src[N1-1]/2;
-//   double b=(Vp[N1-1]-Vp[0]-a*d*d)/d;
-//   double c=Vp[0];
-//   for (size_t i=0; i<N1; i++)
-//      Vp[i] = a*C1[i]*C1[i]+b*C1[i]+c;
-//
-//   for (size_t i=1; i<N1-1; i++)
-//      E1[i]=(Vp[i+1]-Vp[i-1])/(dC1p[i]+dC1m[i]);
-//   E1[0]=(Vp[1]-Vp[0])/dC1p[0];
-//   E1[N1-1]=(Vp[N1-1]-Vp[N1-2])/dC1m[N1-1];
+   for (size_t i=1; i<N1-1; i++) {
+      E1[i]=(Vp[i+1]-Vp[i-1])/(dC1p[i]+dC1m[i]); Et[i]=E1[i];
+   }
+   E1[0]=(Vp[1]-Vp[0])/dC1p[0]; Et[0]=E1[0];
+   E1[N1-1]=(Vp[N1-1]-Vp[N1-2])/dC1m[N1-1]; Et[N1-1]=E1[N1-1];
 }
-//_____________________________________________________________________________
-//
-void X::SetBoundaryCondition(Detector *detector)
-{
-   Printf("%s",detector->ClassName());
-//   if (Height<=0) {
-//      Error("ConfigureX", "Height(%.1fcm)<=0, abort!", Height/cm);
-//      abort();
-//   }
-//   size_t n1 = N1;
-//   for (size_t i=0; i<n1; i++) {
-//      dC1p.push_back(Height/(n1-1)); dC1m.push_back(Height/(n1-1));
-//      C1.push_back(i*dC1p[i]); fIsFixed.push_back(false);
-//   }
-//   // fix 1st and last points
-//   fIsFixed[0]=true; fIsFixed[n1-1]=true;
-//   // linear interpolation between Bias[0] and Bias[1]
-//   double slope = (Bias[1]-Bias[0])/(n1-1);
-//   for (size_t i=0; i<n1-1; i++) Vp[i]=Bias[0]+slope*i;
-//   Vp[n1-1]=Bias[1];
-}
-//_____________________________________________________________________________
-//
--- src/X.h
+++ src/X.h
@@ -9,35 +9,18 @@ namespace GeFiCa { class X; }
 class GeFiCa::X : public GeFiCa::Grid, public TNamed
 {
    public:
-      X(size_t n1=101) : Grid(n1), TNamed("x","1D coordinate") {};
-      void SetBoundaryCondition(Detector *detector);
+      X(size_t n1=101) : Grid(n1), TNamed("x","1D Cartesian coordinate") {};
+      void GetBoundaryConditionFrom(Detector &detector);
       /**
-       * Analytic calculation of  field with fixed impurity concentration.
-       *
-       * In case of fixed impurity, rho, the solution of Poisson's Equation
-       *
-       *     d^2 p / dx^2 = -rho/epsilon
-       *
-       * with boundary conditions:
-       *
-       * - potential(0) = Bias[0],
-       * - potential(d) = Bias[1],
-       *
-       * where, d = UpperBound - LowerBound, is
-       *
-       *     potential(x) = a x^2 + b x + c 
-       *
-       * where, 
-       *
-       * - a = - rho/2/epsilon
-       * - b = (Bias[1]-Bias[0] - ad^2)/d
-       * - c = Bias[0]
+       * Solve Poisson's Equation analytically.
+       * It only accepts a constant impurity throughout the grid.
        */
-      void FillGridWithAnalyticResult();
+      void SolveAnalytically();
 
       ClassDef(X,1);
    protected:
       void OverRelaxAt(size_t idx); 
+      void CalculateE();
 };
 #endif
 
