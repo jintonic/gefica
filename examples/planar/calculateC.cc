eea117e6cda0e17aaef1cc2251ba352070f77f17
Jing LIU <jing.liu@usd.edu>
2019-03-28 09:50:15 -0500

    started to use GetBoundaryConditionFrom

--- examples/planar/calculateC.cc
+++ examples/planar/calculateC.cc
@@ -1,14 +1,15 @@
-// calculate capacitance in unit area of an ideal 1D planar detector
+// calculate capacitance in unit area of an ideal planar detector
 using namespace GeFiCa;
 void calculateC()
 {
-   // configure detector
-   Planar1D *detector = new Planar1D;
-   detector->Thickness=1*cm;
-   detector->V0=0*volt;
-   detector->V1=800*volt;
+   Planar detector;
+   detector.Height=1*cm;
+   detector.Bias[0]=0*volt;
+   detector.Bias[1]=800*volt;
+   detector.SetAverageImpurity(1e10/cm3);
 
-   // calculate capacitance
-   double c = detector->GetC()/pF;
+   X grid;
+   grid.GetBoundaryConditionFrom(detector);
+   double c = grid.GetC()/pF;
    cout<<"capacitance is "<<c<<" pF/cm2"<<endl;
 }
