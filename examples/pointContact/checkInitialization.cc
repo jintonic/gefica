54dcc2111c235480e13a54096598b7ca32f8f36d
Jing LIU <jing.liu@usd.edu>
2019-04-02 17:37:07 -0500

    started to use GetBoundaryConditionFrom

--- examples/pointContact/checkInitialization.cc
+++ examples/pointContact/checkInitialization.cc
@@ -1,12 +1,15 @@
 // show default grid setup of 2D point contact detectors
 {
-   GeFiCa::PointContactDZ pcdz;
-   pcdz.Dump(); // call TObject::Dump() to print data members
-   pcdz.HoleR=5*GeFiCa::mm; pcdz.HoleH=3*GeFiCa::cm; // add bore hole
-   pcdz.HoleTaperW=2*GeFiCa::mm; pcdz.HoleTaperH=2*GeFiCa::mm; // add taper
-   pcdz.WrapAroundR=2*GeFiCa::cm; // add wrap arround
-   pcdz.GrooveW=2*GeFiCa::mm; pcdz.GrooveH=2*GeFiCa::mm; // add groove
-   TTree *t = pcdz.GetTree(); // create a ROOT tree for quick check
+   GeFiCa::PointContact detector;
+   detector.Dump(); // call TObject::Dump() to print data members
+   detector.BoreR=5*GeFiCa::mm; detector.BoreH=3*GeFiCa::cm; // add bore hole
+   detector.BoreTaperW=2*GeFiCa::mm; detector.BoreTaperH=2*GeFiCa::mm; // add taper
+   detector.WrapAroundR=2*GeFiCa::cm; // add wrap arround
+   detector.GrooveW=2*GeFiCa::mm; detector.GrooveH=2*GeFiCa::mm; // add groove
+
+   GeFiCa::RhoZ grid;
+   grid.GetBoundaryConditionFrom(detector);
+   TTree *t = grid.GetTree(); // create a ROOT tree for quick check
    gStyle->SetPadRightMargin(0.12); // give enough space for color palette
    t->Draw("c2:c1:v","","colz"); // visualize initial setup
 }
