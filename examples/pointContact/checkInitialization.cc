686f5399ce4e3544adab35121575a0b7adb698e6
Jing LIU <jing.liu@usd.edu>
2019-04-02 22:12:26 -0500

    set more parameters

--- examples/pointContact/checkInitialization.cc
+++ examples/pointContact/checkInitialization.cc
@@ -1,11 +1,13 @@
 // show default grid setup of 2D point contact detectors
 {
    GeFiCa::PointContact detector;
-   detector.Dump(); // call TObject::Dump() to print data members
    detector.BoreR=5*GeFiCa::mm; detector.BoreH=3*GeFiCa::cm; // add bore hole
-   detector.BoreTaperW=2*GeFiCa::mm; detector.BoreTaperH=2*GeFiCa::mm; // add taper
+   detector.BoreTaperW=2*GeFiCa::mm; detector.BoreTaperH=2*GeFiCa::mm;
+   detector.TaperW=5*GeFiCa::mm; detector.TaperH=4*GeFiCa::mm;
+   detector.CornerW=5*GeFiCa::mm; detector.CornerH=4*GeFiCa::mm;
    detector.WrapAroundR=2*GeFiCa::cm; // add wrap arround
-   detector.GrooveW=2*GeFiCa::mm; detector.GrooveH=2*GeFiCa::mm; // add groove
+   detector.GrooveW=2*GeFiCa::mm; detector.GrooveH=2*GeFiCa::mm;
+   detector.Dump(); // call TObject::Dump() to print data members
 
    GeFiCa::RhoZ grid;
    grid.GetBoundaryConditionFrom(detector);
