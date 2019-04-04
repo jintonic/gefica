ccafc70c9dc1957fc9c0ee0d7b8856df208a1777
rdlab <rdlab@ni13304.usd.local>
2019-04-04 15:34:53 -0500

    fix typo/boundary check

--- src/RhoZ.cc
+++ src/RhoZ.cc
@@ -172,7 +172,14 @@ void RhoZ::ReallocateGridPointsNearBoundaries(PointContact &pc)
       // Fixme: if dC2m is too small, say 1e-5, 1/dC2m becomes too large
       if (C2[i]-pc.PointContactH<dC2m[i] && C2[i]>pc.PointContactH
             && C1[i]<pc.PointContactR && C1[i]>-pc.PointContactR)
+      {
          dC2m[i]=C2[i]-pc.PointContactH; // top of point contact
+         if(dC2m[i]<1e-4)//should check all boundary relocate
+         {
+            Vp[i]=pc.Bias[0];
+            fIsFixed[i]=true;
+         }
+      }
       if (C1[i]-pc.PointContactR<dC1m[i]&&C1[i]>0&&C2[i]<pc.PointContactH)
          dC1m[i]=C1[i]-pc.PointContactR; // right of point contact
       if (-C1[i]-pc.PointContactR<dC1p[i]&&C1[i]<0&&C2[i]<pc.PointContactH)
@@ -209,10 +216,14 @@ void RhoZ::ReallocateGridPointsNearBoundaries(PointContact &pc)
          intercept=-(pc.Radius-pc.TaperW)*slope;
          if (C2[i]-(C1[i]*slope+intercept)<dC2p[i]) {
             dC2m[i]=C2[i]-(slope*C1[i]+intercept);
+         }
+         if (C1[i]-((C2[i]+intercept)/slope)<dC1p[i]) {
             dC1p[i]=C1[i]-intercept/slope-C2[i]/slope; // Fixme: only true when slope>45 deg
          }
          if (C2[i]-(-slope*C1[i]+intercept)<dC2m[i]) {
             dC2m[i]=C2[i]-(-C1[i]*slope+intercept);
+         }
+         if (-C1[i]/slope-intercept/slope-C2[i]<dC2m[i]) {
             dC1m[i]=-C1[i]/slope-intercept/slope-C2[i]; // Fixme: only true when slope>45 deg
          }
       }
