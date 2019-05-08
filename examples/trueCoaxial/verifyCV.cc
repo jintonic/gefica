7572f21d8047fdb517e85bde5f6c693b3047c8f8
Jing LIU <jing.liu@usd.edu>
2019-05-07 23:55:23 -0500

    tried different crystal setup

--- examples/trueCoaxial/verifyCV.cc
+++ examples/trueCoaxial/verifyCV.cc
@@ -1,5 +1,4 @@
-// Compare numerically calculated C-V curves with analytic solution
-
+// Compare numerically calculated C-V curves with analytic solutions
 using namespace GeFiCa;
 const int n=401;
 // get capacitance based on C=2pi*epsilon/ln(radius/boreR), ref. to
@@ -11,23 +10,22 @@ double GetCfromDepletionRadius(double voltage, double radius, double boreR)
    detector.BoreR=boreR;
    detector.Bias[0]=0*volt; // for inner electrode
    detector.Bias[1]=voltage; // for outer electrode
-   detector.SetAverageImpurity(-1e10/cm3); // deplete from inside out
+   detector.SetAverageImpurity(-2e9/cm3); // deplete from inside out
 
    Rho grid(n);
    grid.GetBoundaryConditionFrom(detector);
    grid.RelaxationFactor=1.986;
+   grid.Precision=1e-8;
    grid.SuccessiveOverRelax();
    
    //search for depletion radius
    double up=radius, down=boreR, depletionR=radius;
-   while(up-down>1e-5*mm) {
+   while(up-down>1e-7*mm) {
       depletionR=(down+up)/2;
       if (grid.GetV(depletionR)<voltage) down=depletionR; else up=depletionR;
    }
 
-   cout<<depletionR<<endl;
-   double L = 1*cm;
-   return L*epsilon*3.14159*2/log(depletionR/boreR);
+   return epsilon*3.14159*2/log(depletionR/boreR); // c per unit length in cm
 }
 //______________________________________________________________________________
 // use GefiCa::X::GetC()
@@ -39,50 +37,49 @@ double GetCfromGeFiCa(double voltage, double radius,double boreR)
    detector.BoreR=boreR;
    detector.Bias[0]=0*volt; // for bottom electrode
    detector.Bias[1]=voltage; // for top electrode
-   detector.SetAverageImpurity(-1e10/cm3);
+   detector.SetAverageImpurity(-2e9/cm3);
 
    Rho grid(n);
    grid.GetBoundaryConditionFrom(detector);
    grid.RelaxationFactor=1.986;
+   grid.Precision=1e-8;
+
    return grid.GetC();
 }
 //______________________________________________________________________________
-//
-double FindR(double voltage,double topr,double downr,double innerr, double rho)
+// search for depleted radius recursively
+double FindR(double voltage,double upR,double downR,double boreR,double rho)
 {
-   if (topr<downr+1e-5*mm) return topr; // find the required R
-   double r=(topr+downr)/2;
-   //voltage= -rho*r*r/4 + c1*log(r) + c2, where rho=-1e10/cm3*Qe/epsilon
+   if (upR<downR+1e-7*mm) return upR; // find the required R
+   double r=(upR+downR)/2;
+   //voltage= -rho*r*r/4 + c1*log(r) + c2, where rho=-2e9/cm3*Qe/epsilon
    //c1=-rho*r^2/2 <= when E(r)=0, the detector is depleted from boreR to r
    //c2=rho*boreR*boreR/4 - c1*log(boreR) <= V(boreR)=0
    double c1=-rho*r*r/2;
-   double c2=rho*innerr*innerr/4-c1*log(innerr);
+   double c2=rho*boreR*boreR/4-c1*log(boreR);
    double V=rho*r*r/4+c1*log(r)+c2;
-   if (V>voltage) return FindR(voltage,r,downr,innerr,rho);
-   else return FindR(voltage,topr,r,innerr,rho);
+   if (V>voltage) return FindR(voltage,r,downR,boreR,rho);
+   else return FindR(voltage,upR,r,boreR,rho);
 }
 //______________________________________________________________________________
 //
 double GetCanalytically(double voltage, double radius,double boreR)
 {
-   double rho=-1e10/cm3*Qe/epsilon;
+   double rho=-2e9/cm3*Qe/epsilon;
    double depth=FindR(voltage,radius,boreR,boreR,rho);
    if (depth>radius) depth=radius;
-   cout<<"GetCanalytically, depth: "<<depth<<endl;
-
-   double L = 1*cm;
-   return L*epsilon*3.14159*2/log(depth/boreR);
+   return epsilon*3.14159*2/log(depth/boreR); // c per unit length in cm
 }
 //______________________________________________________________________________
 //
 void verifyCV()
 {
-   double radius=1*cm;
-   double boreR=0.1*cm;
-   const int np=10;
+   double radius=3.5*cm;
+   double boreR=0.5*cm;
+   const int np=25;
    Double_t V[np], Cn[np], Ca[np],Cg[np];
    for (Int_t i=0;i<np;i++) {
-      V[i] = (i+2)*50*volt; Printf("voltage: %.0f V", V[i]);
+      V[i] = (i+2)*100*volt; Printf("voltage: %.0f V", V[i]);
       Cn[i] = GetCfromDepletionRadius(V[i],radius,boreR)/pF;
       Cg[i] = GetCfromGeFiCa(V[i],radius,boreR)/pF;
       Ca[i] = GetCanalytically(V[i],radius,boreR)/pF; 
@@ -99,9 +96,9 @@ void verifyCV()
    gs->SetTitle(";Bias [V];Capacitance per unit length [pF/cm]");
    gs->Draw("pac");
 
-   TLegend *l = new TLegend(0.5,0.6,0.97,0.97);
-   l->AddEntry(ga,"#varepsilon A/d, analytically","pl");
-   l->AddEntry(gn,"#varepsilon A/d, numerically","pl");
+   TLegend *l = new TLegend(0.4,0.7,0.97,0.97);
+   l->AddEntry(ga,"2#pi#varepsilon/ln(R_{out}/R_{in}), analytically","pl");
+   l->AddEntry(gn,"2#pi#varepsilon/ln(R_{out}/R_{in}), numerically","pl");
    l->AddEntry(gg,"GeFiCa::X::GetC()","pl");
    l->Draw();
 }
