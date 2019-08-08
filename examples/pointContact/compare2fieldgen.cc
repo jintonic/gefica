9af4ed7222c1e4b4a9d188f1c6ee0beef1760175
Jing LIU <jing.liu@usd.edu>
2019-08-08 17:51:59 -0500

    made it independent from calculateField.cc

--- examples/pointContact/compare2fieldgen.cc
+++ examples/pointContact/compare2fieldgen.cc
@@ -1,75 +1,82 @@
 using namespace GeFiCa;
-// Compare PointContactDZ to fieldgen
-void drawresult(const char *fieldgen)
+void compare2fieldgen(const char *fieldgenOutput="fieldgen/fields/p1/ev.dat")
 {
-   // draw MJD result
-   TCanvas *c1 = new TCanvas;
-   TTree *tm1 = new TTree("tm1","tm1");
-   tm1->ReadFile(fieldgen, "r:z:v:e:er:ez");
-   tm1->Draw("z:r:v","","colz");
-   //t->AddFriend("t2=t","ppcSOR2.root");
-   //t->Draw("z:(t2.p-v)","z!=1&r!=1&z<1&r>34.&r<34.5","");
-   //TCanvas *can = new TCanvas;
-   //t->Draw("r:(t2.p-v)","z>=0&z<0.2","");
-   //t->Draw("z:r:d","z<50","colz");
+   PointContact detector; // create a point contact detector
+   // same as fieldgen/config_files/p1_new.config
+   detector.Bias[0]=0*kV; // bias on point contact
+   detector.Bias[1]=2.5*kV; // bias on outer contact
+   detector.Radius=34.5*mm; detector.Height=50.5*mm;
+   detector.PointContactR=1.4*mm; detector.PointContactH=2.1*mm;
+   // what's set in fieldgen is space charge instead of impurity density
+   detector.BottomImpurity=0.318e10/cm3; detector.TopImpurity=0.19175e10/cm3;
+   // no fancy feature
+   detector.TaperW=0; detector.TaperH=0;
+   detector.CornerW=0; detector.CornerH=0;
+   detector.BoreH=0; detector.BoreR=0;
+   detector.BoreTaperH=0; detector.BoreTaperW=0;
+   detector.GrooveH=0; detector.GrooveW=0;
+
+   RhoZ grid(690,506); // grid step length is about 0.1 mm
+   grid.GetBoundaryConditionFrom(detector);
+   grid.RelaxationFactor=1.994; // fastest
+   grid.SuccessiveOverRelax();
+
+   // get fieldgen data
+   ifstream fieldgen(fieldgenOutput); if (!fieldgen.is_open()) exit(-1);
+   string str; for (int i=0; i<36; i++) getline(fieldgen,str); // skip header
+
+   // compare
+   ofstream output("gVSf.txt");
+   double r,z,v,e,er,ez,V,E,Er,Ez;
+   while (fieldgen>>r>>z>>v>>e>>er>>ez) {
+      V=grid.GetV(r*mm,z*mm);
+      Er=grid.GetE1(r*mm,z*mm);
+      Ez=grid.GetE2(r*mm,z*mm);
+      E=sqrt(Er*Er+Ez*Ez);
+      output<<r<<"  "<<z<<"  "<<V<<"  "<<v-V<<"  "<<Er<<"  "
+         <<Ez<<"  "<<E<<"  "<<er-Er<<"  "<<ez-Ez<<"  "<<e-E<<endl;
+   }
+   fieldgen.close();
+   output.close();
+
+   gROOT->SetStyle("GeFiCa");
+   gStyle->SetTitleOffset(0.8,"Y");
+   gStyle->SetPadRightMargin(0.15);
+   gStyle->SetPadLeftMargin(0.08);
+
+   // draw fieldgen result
+   TCanvas *c1 = new TCanvas("c1", "fieldgen");
+   TTree *tf = new TTree("tf","tf");
+   tf->ReadFile(fieldgenOutput, "r:z:v:e:er:ez");
+   tf->Draw("z:r:v","","colz");
+
    // draw GeFiCa result
-   TCanvas *c2 = new TCanvas;
+   TCanvas *c2 = new TCanvas("c2", "gefica");
    TTree *tg = new TTree("tg","tg");
    tg->ReadFile("gVSf.txt", "r:z:v:d:e1:e2:e:de1:de2:de");
    tg->Draw("z:r:v","","colz");
 
-   //TCanvas *c4 = new TCanvas;
-   //
-   //c4->SetFillColor(kBlue);
-   //
-   //tg->Draw("z:r:e","","colz");
-   //TCanvas *c5 = new TCanvas;
-   //tg->Draw("z:r:e1","","colz");
-   //TCanvas *c6 = new TCanvas;
-   //tg->Draw("z:r:e2","","colz");
+   // draw difference
+   TCanvas *c3 = new TCanvas("c3", "fieldgen - gefica");
+   tg->Draw("z:r:d","","colz"); // potential difference
 
-   //// draw difference
-   TCanvas *c3 = new TCanvas;
-   tg->Draw("z:r:d","","colz");
-   //TCanvas *c7 = new TCanvas;
-   //tg->Draw("z:r:de1","","colz");
-   //TCanvas *c8 = new TCanvas;
-   //tg->Draw("z:r:de2","","colz");
-   TCanvas *c9 = new TCanvas;
-   //
-   tg->Draw("z:r:de/e*100","(r>1.4 || z>2.22) && (r<34 && z<50)","colz");
-}
+   TCanvas *c4 = new TCanvas("c4","(fieldgen - gefica)/fieldgen*100",600,600);
+   c4->SetLeftMargin(0.1); c4->SetRightMargin(0.2);
+   tg->Draw("z:r:de/e*100","(r>1.4 || z>2.3)","goff"); // field difference
+   TGraph2D *gg = new TGraph2D(tg->GetEntries(),
+         tg->GetV2(), tg->GetV1(), tg->GetV3());
+   gg->SetName("gg"); gg->SetNpx(500); gg->SetNpy(500); // fine bin histogram
+   TH2D *he = gg->GetHistogram();
+   he->SetTitle(";Radius [cm];Height [cm];"
+         "100 #times (E_{fieldgen}-E_{GeFiCa})/E_{fieldgen}");
+   he->GetZaxis()->CenterTitle();
+   he->GetZaxis()->SetTitleOffset(1.5);
+   he->GetYaxis()->SetTitleOffset(0.9);
+   he->Draw("colz2");
 
-//______________________________________________________________________________
-//
-void compare2fieldgen(const char *gefica="ppc.root",
-      const char *fieldgen="ev.dat")
-{
-   TFile *inrootfile=new TFile(gefica);
-   RhoZ *field=(RhoZ*)inrootfile ->Get("grhoz");
+   TBox *box = new TBox(0.05,0,1.42,2.3);
+   box->SetFillColor(kWhite);
+   box->Draw();
 
-   ifstream infile(fieldgen); if (!infile.is_open()) exit(-1);
-   ofstream outfile("gVSf.txt");
-   string str;
-   for (int i=0; i<36; i++) getline(infile,str);
-
-   double x,y,v,er,ez,e,anotherV,E1,E2,E,sizeofr,sizeofz;
-   while (infile>>x>>y>>v>>e>>er>>ez) {
-      sizeofr=x;
-      sizeofz=y;
-      
-      anotherV=field->GetV(x*mm,y*mm);
-/i      if(anotherV<-1000)Printf("x:%f,y:%f,v:%f",x,y,anotherV);
-      E1=field->GetE1(x*mm,y*mm);
-      E2=field->GetE2(x*mm,y*mm);
-      E=sqrt(E1*E1+E2*E2);
-      outfile<<x<<"  "<<y<<"  "<<anotherV<<"  "<<v-anotherV<<"  "<<E1<<"  "<<E2<<"  "<<E<<"  "<<er-E1<<"  "<<ez-E2<<"  "<<e-E<<endl;
-   }
-   infile.close();
-   outfile.close();
-
-   inrootfile->Close();
-   delete inrootfile;
-
-   drawresult(fieldgen);
+   c4->Print("gVSf.png");
 }
